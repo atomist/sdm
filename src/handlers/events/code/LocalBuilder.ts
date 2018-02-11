9f0e85b9e8b136c8cf5bc81d4aebb4e788ed9484
Rod Johnson <rod@atomist.com>
2018-02-11 15:06:17 +1100

    Fixed #2. No hardcoded artifact name

---
+++ src/handlers/events/code/LocalBuilder.ts
@@ -0,0 +1,128 @@
+import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
+import {
+    ProjectOperationCredentials,
+    TokenCredentials,
+} from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
+import { RemoteRepoRef } from "@atomist/automation-client/operations/common/RepoId";
+import { createStatus } from "../../commands/editors/toclient/ghub";
+import { AppInfo } from "./Deployment";
+
+import axios from "axios";
+import { Stream } from "stream";
+import { ArtifactStore, StoredArtifact } from "./ArtifactStore";
+import { ProgressLog } from "./ProgressLog";
+import { Builder, RunningBuild } from "./Builder";
+
+/**
+ * Superclass for build, emitting appropriate events to Atomist
+ */
+export abstract class LocalBuilder implements Builder {
+
+    public build(creds: ProjectOperationCredentials, rr: RemoteRepoRef, team: string, log?: ProgressLog): Promise<RunningBuild> {
+        return this.startBuild(creds, rr, team)
+            .then(rb => {
+                if (!!log) {
+                    // TODO doesn't work
+                    // rb.stream.addListener("message", what => log.write(what.toString()));
+                }
+                //(rb.stream as ChildProcess).on("data", data => output += data.toString());
+
+                //rb.stream.on("end", resolve);
+                rb.stream.addListener("exit", (code, signal) => onExit(code, signal, rb, creds))
+                //.addListener("end", (code, signal) => onExit(code, signal, output))
+                //.addListener("close", (code, signal) => onExit(code, signal, output))
+                    .addListener("error", (code, signal) => onFailure(rb));
+                return rb;
+            })
+            .then(onStarted);
+    }
+
+    protected abstract startBuild(creds: ProjectOperationCredentials, rr: RemoteRepoRef, team: string): Promise<RunningBuild>;
+}
+
+function onStarted(runningBuild: RunningBuild) {
+    return updateAtomistLifecycle(runningBuild, "STARTED", "STARTED");
+}
+
+function onSuccess(runningBuild: RunningBuild) {
+    return updateAtomistLifecycle(runningBuild, "SUCCESS", "FINALIZED");
+    // .then(rb => {
+    //     console.log("Running build AI = " + JSON.stringify(rb.appInfo));
+    //     return rb;
+    // });
+}
+
+function onFailure(runningBuild: RunningBuild) {
+    return updateAtomistLifecycle(runningBuild, "FAILURE", "FINALIZED");
+}
+
+function updateAtomistLifecycle(runningBuild: RunningBuild,
+                                status: "STARTED" | "SUCCESS" | "FAILURE",
+                                phase: "STARTED" | "FINALIZED" = "FINALIZED"): Promise<RunningBuild> {
+    const url = `https://webhook.atomist.com/atomist/jenkins/teams/${runningBuild.team}`;
+    const data = {
+        name: `Build ${runningBuild.repoRef.sha}`,
+        duration: 3,
+        build: {
+            number: "Build",
+            scm: {
+                commit: runningBuild.repoRef.sha,
+                url: `https://github.com/${runningBuild.repoRef.owner}/${runningBuild.repoRef.repo}`,
+                // TODO is this required
+                branch: "master",
+            },
+            phase,
+            status,
+            full_url: `https://github.com/${runningBuild.repoRef.owner}/${runningBuild.repoRef.repo}/commit/${runningBuild.repoRef.sha}`,
+        },
+    };
+    console.log(`BUILD UPDATE: Sending event to Atomist at ${url}\n${JSON.stringify(data)}`);
+    return axios.post(url, data)
+        .then(() => runningBuild);
+}
+
+function onExit(code: number, signal: any, rb: RunningBuild, creds: ProjectOperationCredentials): void {
+    console.log(`BUILD exited with ${code} and signal ${signal}`);
+    //console.log(rb.log);
+    if (code === 0) {
+        onSuccess(rb)
+            .then(id => setArtifact(rb, creds));
+    } else {
+        onFailure(rb);
+    }
+}
+
+export const ScanBase = "https://scan.atomist.com";
+
+export const ArtifactContext = "artifact";
+
+class SimpleArtifactStore implements ArtifactStore {
+
+    public store(appInfo: AppInfo, what: Stream): Promise<string> {
+        console.log("Storing " + JSON.stringify(appInfo));
+        return Promise.resolve("http://www.test.com");
+    }
+
+    public storeFile(appInfo: AppInfo, what: string): Promise<string> {
+        console.log("Storing " + JSON.stringify(appInfo));
+        return Promise.resolve(what);
+    }
+
+    public retrieve(url: string): Promise<StoredArtifact> {
+        return null;
+    }
+}
+
+const artifactStore: ArtifactStore = new SimpleArtifactStore();
+
+function setArtifact(rb: RunningBuild, creds: ProjectOperationCredentials): Promise<any> {
+    // TODO hard coded token must go
+    const id = rb.repoRef as GitHubRepoRef;
+    return artifactStore.storeFile(rb.appInfo, "http://" + rb.deploymentUnitFile)
+        .then(target_url => createStatus((creds as TokenCredentials).token, id, {
+            state: "success",
+            target_url,
+            context: ArtifactContext,
+            //description:
+        }));
+}
--- src/handlers/events/code/MavenBuilder.ts
+++ src/handlers/events/code/MavenBuilder.ts
@@ -2,39 +2,41 @@ import { ProjectOperationCredentials } from "@atomist/automation-client/operatio
 import { RemoteRepoRef } from "@atomist/automation-client/operations/common/RepoId";
 import { GitCommandGitProject } from "@atomist/automation-client/project/git/GitCommandGitProject";
 import { ChildProcess, spawn } from "child_process";
-import * as fs from "fs";
 import { Readable } from "stream";
-import { Builder, LocalBuilder, RunningBuild } from "./Builder";
+import {RunningBuild } from "./Builder";
 import { AppInfo } from "./Deployment";
+import { identification } from "./pomParser";
+import { LocalBuilder } from "./LocalBuilder";
 
 export class MavenBuilder extends LocalBuilder {
 
     protected startBuild(creds: ProjectOperationCredentials, rr: RemoteRepoRef, team: string): Promise<RunningBuild> {
         return GitCommandGitProject.cloned(creds, rr)
             .then(p => {
-                const childProcess = spawn("mvn", [
-                    "package",
-                    "-DskipTests",
-                ], {
-                    cwd: p.baseDir,
-                });
-                const rb = new UpdatingBuild(rr, childProcess, team);
-                childProcess.stdout.on("data", data => {
-                    //console.log("Saw data " + data.to())
-                    rb.l += data.toString();
-                });
-                childProcess.addListener("exit", (code, signal) => {
-                    console.log("Success at " + p.baseDir);
-                    rb.ai = {
-                        // TODO hard coded
-                        name: "test",
-                        version: "1",
-                    };
-                    // TODO this is hard coded
-                    //rb._deploymentUnitStream = fs.createReadStream(`${p.baseDir}/target/losgatos1-0.1.0-SNAPSHOT.jar`);
-                    rb._deploymentUnitFile = `${p.baseDir}/target/losgatos1-0.1.0-SNAPSHOT.jar`;
-                });
-                return rb;
+                // Find the artifact info from Maven
+                return p.findFile("pom.xml")
+                    .then(pom => pom.getContent()
+                        .then(content => identification(content)))
+                    .then(va => ({...va, name: va.artifact}))
+                    .then(appId => {
+                        const childProcess = spawn("mvn", [
+                            "package",
+                            "-DskipTests",
+                        ], {
+                            cwd: p.baseDir,
+                        });
+                        const rb = new UpdatingBuild(rr, childProcess, team);
+                        childProcess.stdout.on("data", data => {
+                            rb.l += data.toString();
+                        });
+                        childProcess.addListener("exit", (code, signal) => {
+                            console.log("Success at " + p.baseDir);
+                            rb.ai = appId;
+                            //rb._deploymentUnitStream = fs.createReadStream(`${p.baseDir}/target/losgatos1-0.1.0-SNAPSHOT.jar`);
+                            rb._deploymentUnitFile = `${p.baseDir}/target/${appId.name}-${appId.version}.jar`;
+                        });
+                        return rb;
+                    });
             });
     }
 
@@ -42,7 +44,8 @@ export class MavenBuilder extends LocalBuilder {
 
 class UpdatingBuild implements RunningBuild {
 
-    constructor(public repoRef: RemoteRepoRef, public stream: ChildProcess, public team: string) {}
+    constructor(public repoRef: RemoteRepoRef, public stream: ChildProcess, public team: string) {
+    }
 
     public l: string = "";
 
