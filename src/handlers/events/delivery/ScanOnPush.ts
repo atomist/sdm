23cd2c6c417d556e19bf40fc653cd1dbe5350774
Rod Johnson <rod@atomist.com>
2018-02-13 10:48:28 +1100

    Externalized scan

--- src/handlers/events/delivery/ScanOnPush.ts
+++ src/handlers/events/delivery/ScanOnPush.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { GraphQL, Secret, Secrets } from "@atomist/automation-client";
+import { GraphQL, Secret, Secrets, success } from "@atomist/automation-client";
 import {
     EventFired,
     EventHandler,
@@ -22,7 +22,6 @@ import {
     HandleEvent,
     HandlerContext,
     HandlerResult,
-    Success,
 } from "@atomist/automation-client/Handlers";
 import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
 import {
@@ -31,9 +30,17 @@ import {
 } from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
 import { GitCommandGitProject } from "@atomist/automation-client/project/git/GitCommandGitProject";
 import { GitProject } from "@atomist/automation-client/project/git/GitProject";
-import { OnPush } from "../../../typings/types";
-import { AddressChannels, addressChannelsFor } from "../../commands/editors/toclient/addressChannels";
+import { OnPush, StatusState } from "../../../typings/types";
+import { addressChannelsFor } from "../../commands/editors/toclient/addressChannels";
 import { createStatus } from "../../commands/editors/toclient/ghub";
+
+export interface ProjectScanResult {
+    passed: boolean;
+    message?: string;
+}
+
+export type ProjectScanner = (p: GitProject,
+                              ctx: HandlerContext) => Promise<ProjectScanResult>;
 
 /**
  * Scan code on a push to master. Result is setting GitHub status with context = "scan"
@@ -41,12 +48,15 @@ import { createStatus } from "../../commands/editors/toclient/ghub";
 @EventHandler("Scan code on PR",
     GraphQL.subscriptionFromFile("../../../../../graphql/subscription/OnPush.graphql",
         __dirname, {
-        branch: "master",
+            branch: "master",
         }))
 export class ScanOnPush implements HandleEvent<OnPush.Subscription> {
 
     @Secret(Secrets.OrgToken)
     private githubToken: string;
+
+    constructor(private projectScanner: ProjectScanner) {
+    }
 
     public handle(event: EventFired<OnPush.Subscription>, ctx: HandlerContext, params: this): Promise<HandlerResult> {
         const push: OnPush.Push = event.data.Push[0];
@@ -57,36 +67,32 @@ export class ScanOnPush implements HandleEvent<OnPush.Subscription> {
 
         const id = new GitHubRepoRef(push.repo.owner, push.repo.name, commit.sha);
 
-        const creds = {token: params.githubToken };
+        const creds = {token: params.githubToken};
 
-        const communicator = addressChannelsFor(push.repo, ctx);
+        const addressChannels = addressChannelsFor(push.repo, ctx);
 
         return GitCommandGitProject.cloned(creds, id)
             .then(p => {
-                return withProject(p, creds, communicator, ctx)
-                    //.then(() => communicator(msg))
-                    .then(() => Success, failure);
+                return params.projectScanner(p, ctx)
+                    .then(scanResult => {
+                        if (scanResult.passed) {
+                            return markScanned(p.id as GitHubRepoRef, "success", creds);
+                        } else {
+                            return addressChannels(`Scan failed: ${scanResult.message}`)
+                                .then(() => markScanned(p.id as GitHubRepoRef, "failure", creds));
+                        }
+                    }).then(() => success());
             });
     }
 }
 
-// TODO have steps and break out if any of them fails
-function withProject(p: GitProject, creds: ProjectOperationCredentials, addressChannels: AddressChannels, ctx: HandlerContext): Promise<any> {
-    return p.findFile("pom.xml")
-        .then(() => markScanned(p.id as GitHubRepoRef, creds))
-        .catch(err => {
-            // This will terminate pipeline
-            console.log("Error was " + err);
-            return addressChannels("This project has no pom. Cannot deploy");
-        });
-}
-
 export const ScanBase = "https://scan.atomist.com";
 
-function markScanned(id: GitHubRepoRef, creds: ProjectOperationCredentials): Promise<any> {
+// TODO this should take a URL with detailed information
+function markScanned(id: GitHubRepoRef, state: StatusState, creds: ProjectOperationCredentials): Promise<any> {
     console.log(`Create status with tok=${JSON.stringify(creds)}`);
     return createStatus((creds as TokenCredentials).token, id, {
-        state: "success",
+        state,
         target_url: `${ScanBase}/${id.owner}/${id.repo}/${id.sha}`,
         context: "scan",
     });
