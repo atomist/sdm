5b67c005a4261e51429a8105a65a19236a2228b7
Rod Johnson <rod@atomist.com>
2018-02-15 22:11:12 +1100

    Upgrade client version and use client's EditOneOrAllParameters

--- src/handlers/commands/editors/registerEditor.ts
+++ src/handlers/commands/editors/registerEditor.ts
@@ -1,11 +1,11 @@
 import { HandleCommand } from "@atomist/automation-client";
 import { allReposInTeam } from "@atomist/automation-client/operations/common/allReposInTeamRepoFinder";
 import { gitHubRepoLoader } from "@atomist/automation-client/operations/common/gitHubRepoLoader";
+import { EditOneOrAllParameters } from "@atomist/automation-client/operations/common/params/EditOneOrAllParameters";
 import { PullRequest } from "@atomist/automation-client/operations/edit/editModes";
 import { EditorCommandDetails, editorHandler } from "@atomist/automation-client/operations/edit/editorToCommand";
 import { AnyProjectEditor } from "@atomist/automation-client/operations/edit/projectEditor";
 import { DefaultDirectoryManager } from "@atomist/automation-client/project/git/GitCommandGitProject";
-import { EditOneOrAllParameters } from "./toclient/EditOneOrAllParameters";
 
 // TODO should this kind of thing go in a library? Not yet
 
@@ -16,9 +16,10 @@ import { EditOneOrAllParameters } from "./toclient/EditOneOrAllParameters";
  * @param {Partial<EditorCommandDetails>} details
  * @return {HandleCommand<EditOneOrAllParameters>}
  */
-export function editor<PARAMS extends EditOneOrAllParameters = EditOneOrAllParameters>(edd: (params: PARAMS) => AnyProjectEditor,
-                                                                                       name: string,
-                                                                                       details: Partial<EditorCommandDetails> = {}): HandleCommand<EditOneOrAllParameters> {
+export function editor<PARAMS extends EditOneOrAllParameters =
+    EditOneOrAllParameters>(edd: (params: PARAMS) => AnyProjectEditor,
+                            name: string,
+                            details: Partial<EditorCommandDetails> = {}): HandleCommand<EditOneOrAllParameters> {
 
     const description = details.description || name;
     const detailsToUse: EditorCommandDetails = {
