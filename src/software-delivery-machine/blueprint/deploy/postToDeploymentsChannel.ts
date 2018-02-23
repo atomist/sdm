5d3f4191e47252254a0a5d3695956d79786c3c64
Rod Johnson <rod@atomist.com>
2018-02-23 12:39:22 -0800

    Fix #51

--- src/software-delivery-machine/blueprint/deploy/postToDeploymentsChannel.ts
+++ src/software-delivery-machine/blueprint/deploy/postToDeploymentsChannel.ts
@@ -4,3 +4,8 @@ export const PostToDeploymentsChannel: DeployListener =
     (id, status, ac, ctx) => {
         return ctx.messageClient.addressChannels(`Successful deployment of \`${id.owner}/${id.repo}:${id.sha}\``, "deployments");
     };
+
+export const PostToServiceChannel: DeployListener =
+    (id, status, ac, ctx) => {
+        return ac(`Whoopee! I was successful deployed!`);
+    };
