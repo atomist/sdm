5b0471ff74e126a1f3a4c89e1cd17bb13416b276
Jessica Kerr <jessitron@gmail.com>
2018-03-01 11:08:08 -0600

    Make one FailDownstream work for all
    This gets the phases off the commit, and orders them according to convention

--- src/handlers/events/delivery/FailDownstreamPhasesOnPhaseFailure.ts
+++ src/handlers/events/delivery/FailDownstreamPhasesOnPhaseFailure.ts
@@ -14,12 +14,16 @@
  * limitations under the License.
  */
 
-import { GraphQL, HandlerResult, logger, Secret, Secrets, Success } from "@atomist/automation-client";
-import { EventFired, EventHandler, HandleEvent, HandlerContext } from "@atomist/automation-client/Handlers";
-import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
-import { Phases } from "../../../common/phases/Phases";
-import { OnFailureStatus, OnSuccessStatus } from "../../../typings/types";
+import {GraphQL, HandlerResult, logger, Secret, Secrets, Success} from "@atomist/automation-client";
+import {EventFired, EventHandler, HandleEvent, HandlerContext} from "@atomist/automation-client/Handlers";
+import {GitHubRepoRef} from "@atomist/automation-client/operations/common/GitHubRepoRef";
+import {Phases, PlannedPhase} from "../../../common/phases/Phases";
+import {OnFailureStatus, OnSuccessStatus} from "../../../typings/types";
 import Status = OnSuccessStatus.Status;
+import {BaseContext, contextIsAfter, GitHubStatusContext, splitContext} from "../../../common/phases/gitHubContext";
+import {ProjectOperationCredentials, TokenCredentials} from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
+import {createStatus, State} from "../../../util/github/ghub";
+import {contextToPlannedPhase, ContextToPlannedPhase} from "./phases/httpServicePhases";
 
 /**
  * Respond to a failure status by failing downstream phases
@@ -31,9 +35,6 @@ export class FailDownstreamPhasesOnPhaseFailure implements HandleEvent<OnFailure
     @Secret(Secrets.OrgToken)
     private githubToken: string;
 
-    constructor(private phases: Phases) {
-    }
-
     public handle(event: EventFired<OnFailureStatus.Subscription>, ctx: HandlerContext, params: this): Promise<HandlerResult> {
         const status: Status = event.data.Status[0];
         const commit = status.commit;
@@ -43,15 +44,60 @@ export class FailDownstreamPhasesOnPhaseFailure implements HandleEvent<OnFailure
             return Promise.resolve(Success);
         }
 
+        if (status.description.startsWith("Skipping ")) {
+            logger.debug("not relevant, because I set this status to failure in an earlier invocation of myself.");
+            logger.debug(`context: ${status.context} description: ${status.description}`);
+            return Promise.resolve(Success);
+        }
+
         const currentlyPending = status.commit.statuses.filter(s => s.state === "pending");
 
         const id = new GitHubRepoRef(commit.repo.owner, commit.repo.name, commit.sha);
 
-        return params.phases.gameOver(
+        return gameOver(
             status.context,
             currentlyPending.map(s => s.context),
             id,
             {token: params.githubToken})
             .then(() => Success);
     }
+}
+
+/**
+ * Set all downstream phase to failure status given a specific failed phase.
+ *
+ * The phases are associated by the atomist-sdm/${env}/ prefix in their context.
+ *
+ */
+function gameOver(failedContext: GitHubStatusContext, currentlyPending: GitHubStatusContext[],
+                  id: GitHubRepoRef, creds: ProjectOperationCredentials): Promise<any> {
+
+    const interpretedContext = splitContext(failedContext);
+
+    if (!interpretedContext) {
+        // this is not our status
+        logger.info("not relevant: " + failedContext);
+        return Promise.resolve();
+    }
+
+    const failedPhase: PlannedPhase = contextToPlannedPhase(failedContext);
+
+    const phasesToReset = currentlyPending
+        .filter(pendingContext => contextIsAfter(failedContext, pendingContext))
+        .map(p => contextToPlannedPhase(p));
+    return Promise.all(phasesToReset.map(
+        p => setStatus(id, p.context, "failure", creds,
+            `Skipping ${p.name} because ${failedPhase.name} failed`)));
+}
+
+function setStatus(id: GitHubRepoRef, context: GitHubStatusContext,
+                   state: State,
+                   creds: ProjectOperationCredentials,
+                   description: string = context): Promise<any> {
+    logger.debug("Setting pending status " + context + " to failure");
+    return createStatus((creds as TokenCredentials).token, id, {
+        state,
+        context,
+        description,
+    });
 }
