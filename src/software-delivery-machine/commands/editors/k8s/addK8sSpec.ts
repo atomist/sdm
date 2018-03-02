cb2ede2f49e89e8329d696e1ae99dfb36b4c22bb
Jessica Kerr <jessitron@gmail.com>
2018-03-02 11:59:42 -0600

    Add k8s, make PR more communicative

--- src/software-delivery-machine/commands/editors/k8s/addK8sSpec.ts
+++ src/software-delivery-machine/commands/editors/k8s/addK8sSpec.ts
@@ -2,6 +2,7 @@ import { HandleCommand, logger } from "@atomist/automation-client";
 import { SimpleProjectEditor } from "@atomist/automation-client/operations/edit/projectEditor";
 import { editor } from "../../../../handlers/commands/editors/registerEditor";
 import { deploymentTemplate } from "./deploymentTemplate";
+import { PullRequest } from "@atomist/automation-client/operations/edit/editModes";
 
 export const AddK8sSpecCommandName = "AddK8sSpecCommandName";
 
@@ -11,7 +12,15 @@ export const ImagePlaceholder = "image_placeholder";
 
 export const addK8sSpec: HandleCommand<any> = editor(
     () => addK8sSpecEditor,
-    AddK8sSpecCommandName);
+    AddK8sSpecCommandName, {
+        editMode: new PullRequest("enable-k8s",
+            "Enable deployment to Kubernetes",
+            `This will trigger the Software Development Machine to request deployment of this service in a Kubernetes environment.
+
+The Atomist playground kubernetes environment is restricted, so changes to this file will not change anything there.
+In your own kubernetes environment, you can do what you like; see https://github.com/atomist/k8-automation for a starting point.`,
+            "enable deployment to kubernetes")
+    });
 
 export const addK8sSpecEditor: SimpleProjectEditor = async (p, ctx) => {
     const specFileContent = deploymentTemplate(p.id.repo, p.id.owner, p.id.repo,
