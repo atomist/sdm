ccf3cbb54175b4e2fc686259e888845845afbc6b
Jessica Kerr <jessitron@gmail.com>
2018-03-04 10:45:32 +0000

    some code is simpler with promises

--- src/common/listener/support/jvmGuards.ts
+++ src/common/listener/support/jvmGuards.ts
@@ -1,15 +1,9 @@
-
 import { SpringBootProjectStructure } from "@atomist/spring-automation/commands/generator/spring/SpringBootProjectStructure";
 import { PushTest } from "../PhaseCreator";
 
-export const IsMaven: PushTest = async pi => {
-    try {
-        await pi.project.findFile("pom.xml");
-        return true;
-    } catch {
-        return false;
-    }
-};
+export const IsMaven: PushTest = pi =>
+    pi.project.findFile("pom.xml")
+        .then(() => true, () => false);
 
 export const IsSpringBoot: PushTest = async pi => {
     const springBootStructure = await SpringBootProjectStructure.inferFromJavaSource(pi.project);
--- src/common/listener/support/k8sSpecPushTest.ts
+++ src/common/listener/support/k8sSpecPushTest.ts
@@ -1,11 +1,6 @@
 import { AtomistK8sSpecFile } from "../../../software-delivery-machine/commands/editors/k8s/addK8sSpec";
 import { PushTest } from "../PhaseCreator";
 
-export const HasK8Spec: PushTest = async inv => {
-    try {
-        await inv.project.findFile(AtomistK8sSpecFile);
-        return true;
-    } catch {
-        return false;
-    }
-};
+export const HasK8Spec: PushTest = inv =>
+    inv.project.findFile(AtomistK8sSpecFile)
+        .then(() => true, () => false);
