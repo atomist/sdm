66f68a57da075cb41ecb58600ddb4458d44f7a5f
Rod Johnson <rod@atomist.com>
2018-03-04 17:11:05 +0000

    Update K8 machine

--- src/software-delivery-machine/k8sSoftwareDeliveryMachine.ts
+++ src/software-delivery-machine/k8sSoftwareDeliveryMachine.ts
@@ -4,18 +4,25 @@ import { IsMaven, IsSpringBoot } from "../common/listener/support/jvmGuards";
 import { HasK8Spec } from "../common/listener/support/k8sSpecPushTest";
 import { MaterialChangeToJavaRepo } from "../common/listener/support/materialChangeToJavaRepo";
 import { IsNode } from "../common/listener/support/nodeGuards";
-import { PushesToDefaultBranch, PushToPublicRepo } from "../common/listener/support/pushTests";
-import { HttpServicePhases, LocalDeploymentPhases } from "../handlers/events/delivery/phases/httpServicePhases";
+import { PushesToDefaultBranch, PushFromAtomist, PushToPublicRepo } from "../common/listener/support/pushTests";
+import {
+    HttpServicePhases,
+    ImmaterialPhases,
+    LocalDeploymentPhases
+} from "../handlers/events/delivery/phases/httpServicePhases";
 import { LibraryPhases } from "../handlers/events/delivery/phases/libraryPhases";
 import { NpmPhases } from "../handlers/events/delivery/phases/npmPhases";
 import { K8sBuildOnSuccessStatus } from "./blueprint/build/K8sBuildOnScanSuccess";
 import {
-    K8sProductionDeployOnSuccessStatus, K8sStagingDeployOnSuccessStatus, NoticeK8sProdDeployCompletion,
+    K8sProductionDeployOnSuccessStatus,
+    K8sStagingDeployOnSuccessStatus,
+    NoticeK8sProdDeployCompletion,
     NoticeK8sTestDeployCompletion,
 } from "./blueprint/deploy/k8sDeploy";
 import { suggestAddingK8sSpec } from "./blueprint/repo/suggestAddingK8sSpec";
 import { addK8sSpec } from "./commands/editors/k8s/addK8sSpec";
 import { configureSpringSdm } from "./springSdmConfig";
+import { not } from "../common/listener/support/pushTestUtils";
 
 export function k8sSoftwareDeliveryMachine(opts: { useCheckstyle: boolean }): SoftwareDeliveryMachine {
     const sdm = new SoftwareDeliveryMachine(
@@ -26,10 +33,14 @@ export function k8sSoftwareDeliveryMachine(opts: { useCheckstyle: boolean }): So
                 K8sProductionDeployOnSuccessStatus,
             ],
         },
-        new GuardedPhaseCreator(HttpServicePhases, IsMaven, HasK8Spec, PushesToDefaultBranch, PushToPublicRepo),
-        new GuardedPhaseCreator(LocalDeploymentPhases, IsMaven, IsSpringBoot),
+        new GuardedPhaseCreator(ImmaterialPhases, IsMaven, IsSpringBoot, not(MaterialChangeToJavaRepo)),
+        new GuardedPhaseCreator(HttpServicePhases, PushesToDefaultBranch, IsMaven, IsSpringBoot,
+            HasK8Spec,
+            PushToPublicRepo),
+        new GuardedPhaseCreator(LocalDeploymentPhases, not(PushFromAtomist), IsMaven, IsSpringBoot),
+        new GuardedPhaseCreator(LibraryPhases, IsMaven, MaterialChangeToJavaRepo),
         new GuardedPhaseCreator(NpmPhases, IsNode),
-        new GuardedPhaseCreator(LibraryPhases, IsMaven, PushesToDefaultBranch));
+    );
     sdm.addNewRepoWithCodeActions(suggestAddingK8sSpec);
     sdm.addSupportingCommands(() => addK8sSpec);
     sdm.addSupportingEvents(() => NoticeK8sTestDeployCompletion,
