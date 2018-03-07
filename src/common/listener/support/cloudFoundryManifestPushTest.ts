6abaec538ec53e2674eaf3fe63660cd0db14086d
Rod Johnson <rod@atomist.com>
2018-03-07 08:32:38 +0000

    Simpler test for cloud foundry manifest

--- src/common/listener/support/cloudFoundryManifestPushTest.ts
+++ src/common/listener/support/cloudFoundryManifestPushTest.ts
@@ -2,11 +2,7 @@
 import { CloudFoundryManifestPath } from "../../../handlers/events/delivery/deploy/pcf/CloudFoundryTarget";
 import { PushTest } from "../PhaseCreator";
 
-export const HasCloudFoundryManifest: PushTest = async inv => {
-    try {
-        await inv.project.findFile(CloudFoundryManifestPath);
-        return true;
-    } catch {
-        return false;
-    }
-};
+import { fileExists } from "@atomist/automation-client/project/util/projectUtils";
+
+export const HasCloudFoundryManifest: PushTest = async inv =>
+    fileExists(inv.project, CloudFoundryManifestPath, f => true);
