edceb9a5e44ae91b33bfde44f741038aa30e9138
Jessica Kerr <jessitron@gmail.com>
2018-03-07 12:03:37 +0000

    Send Atomist team ID for builds, slack team for messages

--- src/common/slack/addressChannels.ts
+++ src/common/slack/addressChannels.ts
@@ -13,7 +13,7 @@ import { SlackMessage } from "@atomist/slack-messages";
 export type AddressChannels = (msg: string | SlackMessage, opts?: MessageOptions) => Promise<any>;
 
 export interface HasChannels {
-    channels?: Array<{ name?: string, id?: string }>;
+    channels?: Array<{ name?: string, id?: string, team?: { id?: string }}>;
 }
 
 export function addressChannelsFor(hasChannels: HasChannels, ctx: HandlerContext): AddressChannels {
@@ -25,8 +25,12 @@ export function addressChannelsFor(hasChannels: HasChannels, ctx: HandlerContext
 }
 
 export function messageDestinations(hasChannels: HasChannels, ctx: HandlerContext): Destination {
-    const channels = hasChannels.channels.map(c => c.name);
-    return addressSlackChannels(ctx.teamId, ...channels);
+    const channelNames = hasChannels.channels.map(c => c.name);
+    if (hasChannels.channels.length === 0) {
+        throw new Error("I can't give you destinations for 0 channels");
+    }
+    const slackTeam = hasChannels[0].channels.team.id;
+    return addressSlackChannels(slackTeam, ...channelNames);
 }
 
 export function addressDestination(destination: Destination, ctx: HandlerContext): AddressChannels {
