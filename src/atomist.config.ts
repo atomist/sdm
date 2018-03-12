55f358b0e0089726466d6cc1b83c6123779fc872
Christian Dupuis <cd@atomist.com>
2018-03-12 09:51:02 +0100

    fix instructions to obtain correct teamId

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -4,7 +4,10 @@ import { ComposedFunctionalUnit } from "./blueprint/ComposedFunctionalUnit";
 import { applyHttpServiceGoals } from "./software-delivery-machine/blueprint/goal/jvmGoalManagement";
 import { cloudFoundrySoftwareDeliveryMachine } from "./software-delivery-machine/cloudFoundrySoftwareDeliveryMachine";
 import { affirmationEditor } from "./software-delivery-machine/commands/editors/affirmationEditor";
-import { breakBuildEditor, unbreakBuildEditor } from "./software-delivery-machine/commands/editors/breakBuild";
+import {
+    breakBuildEditor,
+    unbreakBuildEditor,
+} from "./software-delivery-machine/commands/editors/breakBuild";
 import {
     javaAffirmationEditor,
     javaBranchAffirmationEditor,
@@ -17,16 +20,18 @@ const pj = require(`${appRoot.path}/package.json`);
 const token = process.env.GITHUB_TOKEN;
 
 const assembled = new ComposedFunctionalUnit(
-       cloudFoundrySoftwareDeliveryMachine({useCheckstyle: process.env.USE_CHECKSTYLE === "true"}),
-      // k8sSoftwareDeliveryMachine({useCheckstyle: process.env.USE_CHECKSTYLE === "true"}),
+       cloudFoundrySoftwareDeliveryMachine({ useCheckstyle: process.env.USE_CHECKSTYLE === "true" }),
+      // k8sSoftwareDeliveryMachine({ useCheckstyle: process.env.USE_CHECKSTYLE === "true" }),
 );
 
 export const configuration: Configuration = {
     name: pj.name,
     version: pj.version,
+    // <-- obtain the ID from the settings page of your Atomist workspace at https://app.atomist.com,
+    // then set your env variable
     teamIds: [
          process.env.ATOMIST_WORKSPACE,
-    ], // <-- run @atomist pwd in your slack team to obtain the team id, then set your env variable
+    ],
     commands: assembled.commandHandlers.concat([
         () => affirmationEditor,
         () => applyHttpServiceGoals,
