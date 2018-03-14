8a66a598f7e64944e23e2a857693aa10ebed788b
Rod Johnson <rod@atomist.com>
2018-03-14 13:57:09 +1100

    Support both old and new style Cloud Foundry endpoint exposure

--- src/handlers/events/delivery/deploy/pcf/cloudFoundryLogParser.ts
+++ src/handlers/events/delivery/deploy/pcf/cloudFoundryLogParser.ts
@@ -3,12 +3,10 @@ import { isPatternMatch } from "@atomist/microgrammar/PatternMatch";
 
 /**
  * Use a microgrammar to parse the Cloud Foundry log to extract the endpoint
- * url if found
- * @param {string} cfLog
- * @return {DeploymentInfo}
+ * url if found. Look for urls: or routes: style exposure in log.
  */
 export function parseCloudFoundryLogForEndpoint(cfLog: string): string | undefined {
-    const r = mg.firstMatch(cfLog);
+    const r = routes.firstMatch(cfLog) || urls.firstMatch(cfLog);
     if (isPatternMatch(r)) {
         if (!r.endpoint.startsWith("http://")) {
             return "http://" + r.endpoint;
@@ -19,8 +17,16 @@ export function parseCloudFoundryLogForEndpoint(cfLog: string): string | undefin
 }
 
 // The cf cli changed from returning urls to routes in a recent version
-const mg = Microgrammar.fromString<{endpoint: string}>(
+const routes = Microgrammar.fromString<{endpoint: string}>(
     "routes:${endpoint}",
+    {
+        endpoint: /[http:\/\/]?[a-zA-Z0-9\-.]+/,
+    },
+);
+
+// Old style urls value
+const urls = Microgrammar.fromString<{endpoint: string}>(
+    "urls:${endpoint}",
     {
         endpoint: /[http:\/\/]?[a-zA-Z0-9\-.]+/,
     },
