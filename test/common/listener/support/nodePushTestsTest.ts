778080377114619c4245461e380722cc61165f29
Rod Johnson <rod@atomist.com>
2018-03-24 09:48:57 +1100

    Added nodePushTestTests

---
+++ test/common/listener/support/nodePushTestsTest.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { InMemoryProject } from "@atomist/automation-client/project/mem/InMemoryProject";
+import "mocha";
+import * as assert from "power-assert";
+import { ProjectListenerInvocation } from "../../../../src/common/listener/Listener";
+import { IsJava, IsMaven } from "../../../../src/common/listener/support/pushtest/jvm/jvmPushTests";
+import { IsNode } from "../../../../src/common/listener/support/pushtest/node/nodePushTests";
+import { IsTypeScript } from "../../../../src/common/listener/support/pushtest/node/tsPushTests";
+
+describe("nodePushTests", () => {
+
+    describe("IsTypeScript", () => {
+
+        it("should not find ts in empty repo", async () => {
+            const project = InMemoryProject.of();
+            const r = await IsTypeScript.test({project} as any as ProjectListenerInvocation);
+            assert(!r);
+        });
+
+        it("should find ts in repo with ts file", async () => {
+            const project = InMemoryProject.of({ path: "src/Thing.ts", content: "export class Thing {}"});
+            const r = await IsTypeScript.test({project} as any as ProjectListenerInvocation);
+            assert(r);
+        });
+
+        it("should not find ts in repo with no ts file", async () => {
+            const project = InMemoryProject.of({ path: "src/main/java/Thing.kt", content: "public class Thing {}"});
+            const r = await IsTypeScript.test({project} as any as ProjectListenerInvocation);
+            assert(!r);
+        });
+    });
+
+});
