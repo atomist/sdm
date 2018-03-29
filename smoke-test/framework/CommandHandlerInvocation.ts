dfc9103efeafa2c7e2d3e41a26034d31c5ac3697
Rod Johnson <rod@atomist.com>
2018-03-29 15:06:11 +1100

    Make framework for editor testing and extract smoke test framework code

---
+++ smoke-test/framework/CommandHandlerInvocation.ts
@@ -0,0 +1,48 @@
+
+import { automationServerAuthHeaders, SmokeTestConfig } from "../config";
+import { logger } from "@atomist/automation-client";
+import { Arg, Secret } from "@atomist/automation-client/internal/invoker/Payload";
+
+import axios from "axios";
+import * as _ from "lodash";
+
+export interface CommandHandlerInvocation {
+    name: string;
+    parameters: Arg[];
+    mappedParameters?: Arg[];
+    secrets?: Secret[];
+}
+
+export async function invokeCommandHandler(config: SmokeTestConfig,
+                                           invocation: CommandHandlerInvocation) {
+    const url = `${config.baseEndpoint}/command/${_.kebabCase(invocation.name)}`;
+    const data = {
+        parameters: invocation.parameters,
+        mapped_parameters: invocation.mappedParameters,
+        secrets: invocation.secrets,
+        command: invocation.name,
+    };
+    logger.info(`Hitting ${url} to test command ${invocation.name} with payload ${JSON.stringify(data)}`);
+    const resp = await axios.post(url, data, automationServerAuthHeaders(config));
+
+    // tslint:disable-next-line:no-console
+    console.log("RESP was " + resp.status);
+    return resp.data;
+}
+
+export function editorOneInvocation(editorCommandName: string,
+                                    owner: string,
+                                    repo: string,
+                                    parameters: Arg[] = []): CommandHandlerInvocation {
+    return {
+        name: editorCommandName,
+        parameters,
+        mappedParameters: [
+            {name: "targets.owner", value: owner },
+            {name: "targets.repo", value: repo},
+        ],
+        secrets: [
+            {uri: "github://user_token?scopes=repo,user:email,read:user", value: process.env.GITHUB_TOKEN},
+        ],
+    };
+}
--- smoke-test/testAgainstExistingProject.ts
+++ smoke-test/testAgainstExistingProject.ts
@@ -1,32 +1,14 @@
-import { logger } from "@atomist/automation-client";
-/*
- * Copyright © 2018 Atomist, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import "mocha";
-import { automationServerAuthHeaders, DefaultSmokeTestConfig, SmokeTestConfig } from "./config";
-
-import { Arg, Secret } from "@atomist/automation-client/internal/invoker/Payload";
+import { DefaultSmokeTestConfig, SmokeTestConfig } from "./config";
 import * as assert from "assert";
-import axios from "axios";
 import * as stringify from "json-stringify-safe";
-import * as _ from "lodash";
 import { SelfDescribeCommandName } from "../src/handlers/commands/SelfDescribe";
 import { AffirmationEditorName } from "../src/software-delivery-machine/commands/editors/demo/affirmationEditor";
+import { editorOneInvocation, invokeCommandHandler } from "./framework/CommandHandlerInvocation";
 
 const testConfig: SmokeTestConfig = DefaultSmokeTestConfig;
+
+const GithubOrg = "spring-team";
 
 describe("local SDM", () => {
 
@@ -45,46 +27,10 @@ describe("local SDM", () => {
     describe("test against existing project", () => {
 
         it("changes readme", async () => {
-            // TODO factor out editor parameters
-            const handlerResult = await invokeCommandHandler(testConfig, {
-                name: AffirmationEditorName,
-                parameters: [
-                ],
-                mappedParameters: [
-                    // TODO get rid of hard coding of owning team
-                    { name: "targets.owner", value: "spring-team"},
-                    { name: "targets.repo", value: "losgatos1"},
-                ],
-                secrets: [
-                    { uri: "github://user_token?scopes=repo,user:email,read:user", value: process.env.GITHUB_TOKEN},
-                ],
-            });
-        });
+            const handlerResult = await invokeCommandHandler(testConfig,
+                editorOneInvocation(AffirmationEditorName, GithubOrg, "losgatos1"));
+        }).timeout(10000);
 
     });
 
 });
-
-export interface CommandHandlerInvocation {
-    name: string;
-    parameters: Arg[];
-    mappedParameters?: Arg[];
-    secrets?: Secret[];
-}
-
-export async function invokeCommandHandler(config: SmokeTestConfig,
-                                           invocation: CommandHandlerInvocation) {
-    const url = `${testConfig.baseEndpoint}/command/${_.kebabCase(invocation.name)}`;
-    const data = {
-        parameters: invocation.parameters,
-        mapped_parameters: invocation.mappedParameters,
-        secrets: invocation.secrets,
-        command: invocation.name,
-    };
-    logger.info(`Hitting ${url} to test command ${invocation.name} with payload ${JSON.stringify(data)}`);
-    const resp = await axios.post(url, data, automationServerAuthHeaders(testConfig));
-
-    // tslint:disable-next-line:no-console
-    console.log("RESP was " + resp.status);
-    return resp.data;
-}
