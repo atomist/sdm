e24ab10fe018db6fe43fa81d33cd5ce4c82983ad
Jessica Kerr <jessitron@gmail.com>
2018-04-02 15:12:36 +1000

    move storeGoals call out of Goals

---
+++ test/blueprint/softwareDeliveryMachineTest.ts
@@ -0,0 +1,11 @@
+import "mocha";
+import * as assert from "power-assert";
+
+describe("implementing goals in the SDM", () => {
+
+    it("knows the implementation of a standard goal", () => {
+
+    });
+
+
+});
