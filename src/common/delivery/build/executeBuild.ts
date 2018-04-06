270d3c27547a48f2005d31b1a0e02bf5e6e33ff9
Rod Johnson <rod@atomist.com>
2018-04-06 19:50:03 +1000

    Fixed compile error

--- src/common/delivery/build/executeBuild.ts
+++ src/common/delivery/build/executeBuild.ts
@@ -14,16 +14,11 @@
  * limitations under the License.
  */
 
-import { HandlerContext, logger, Success } from "@atomist/automation-client";
-import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
+import { logger } from "@atomist/automation-client";
 import { Builder } from "../../../spi/build/Builder";
-import { OnAnyPendingStatus } from "../../../typings/types";
-import { ProjectListenerInvocation } from "../../listener/Listener";
-import { PushMapping } from "../../listener/PushMapping";
 import { ProjectLoader } from "../../repo/ProjectLoader";
-import { addressChannelsFor } from "../../slack/addressChannels";
 import { ExecuteGoalWithLog, RunWithLogContext } from "../deploy/runWithLog";
-import { ExecuteGoalInvocation, ExecuteGoalResult, GoalExecutor } from "../goals/goalExecution";
+import { ExecuteGoalResult } from "../goals/goalExecution";
 
 /**
  * Execute build with the appropriate builder
