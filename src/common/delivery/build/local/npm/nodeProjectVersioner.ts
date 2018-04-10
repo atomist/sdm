22682e7bbaf7e164d7fdfc2770832f91d96f8ccb
Christian Dupuis <cd@atomist.com>
2018-04-11 00:31:40 +0200

    set version in the project too

--- src/common/delivery/build/local/npm/nodeProjectVersioner.ts
+++ src/common/delivery/build/local/npm/nodeProjectVersioner.ts
@@ -15,10 +15,25 @@
  */
 
 import * as df from "dateformat";
+import { spawnAndWatch } from "../../../../../util/misc/spawned";
 import { ProjectVersioner } from "../projectVersioner";
 
-export const NodeProjectVersioner: ProjectVersioner = async p => {
+export const NodeProjectVersioner: ProjectVersioner = async (p, log) => {
     const pjFile = await p.getFile("package.json");
     const pj = JSON.parse(await pjFile.getContent());
-    return `${pj.version}-${df(new Date(), "yyyymmddHHMMss")}`;
+    const version = `${pj.version}-${df(new Date(), "yyyymmddHHMMss")}`;
+
+    await spawnAndWatch({
+            command: "npm",
+            args: ["--no-git-tag-version", "version", version],
+        },
+        {
+            cwd: p.baseDir,
+        },
+        log,
+        {
+            errorFinder: code => code !== 0,
+        });
+
+    return version;
 };
