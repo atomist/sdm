b47805b6a01f8045721eb33ac720a329bb0c9435
Christian Dupuis <cd@atomist.com>
2018-04-12 22:53:15 +0200

    fix superseeded sha
    fixes #307

--- src/handlers/events/delivery/superseded/SetSupersededStatus.ts
+++ src/handlers/events/delivery/superseded/SetSupersededStatus.ts
@@ -49,7 +49,7 @@ export class SetSupersededStatus implements HandleEvent<OnPushWithBefore.Subscri
         createStatus(params.githubToken, id, {
             context: SupersededContext,
             state: "error",
-            description: `Superseded by \`${push.after.sha.substring(0, 6)}\`: _${truncateCommitMessage(push.after.message, push.repo)}_`,
+            description: `Superseded by \`${push.after.sha.substring(0, 7)}\`: _${truncateCommitMessage(push.after.message, push.repo)}_`,
             target_url: `${id.url}/tree/${id.sha}`,
         });
 
