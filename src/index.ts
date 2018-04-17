f1fb6ee4030dbad9070b757a644b0ce01adbbee0
Rod Johnson <rod@atomist.com>
2018-04-17 11:21:28 +1000

    Update barrel

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright © 2018 Atomist, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 export * from "./blueprint/FunctionalUnit";
 export * from "./blueprint/SoftwareDeliveryMachine";
 export * from "./blueprint/dsl/goalDsl";
@@ -46,6 +30,7 @@ export * from "./common/delivery/build/local/maven/artifact";
 export * from "./common/delivery/build/local/maven/mavenLogInterpreter";
 export * from "./common/delivery/build/local/maven/pomParser";
 export * from "./common/delivery/build/local/npm/NpmDetectBuildMapping";
+export * from "./common/delivery/build/local/npm/executePublish";
 export * from "./common/delivery/build/local/npm/nodeProjectIdentifier";
 export * from "./common/delivery/build/local/npm/nodeProjectVersioner";
 export * from "./common/delivery/build/local/npm/npmBuilder";
@@ -62,6 +47,7 @@ export * from "./common/delivery/code/fingerprint/executeFingerprinting";
 export * from "./common/delivery/code/fingerprint/maven/MavenFingerprinter";
 export * from "./common/delivery/code/fingerprint/maven/dependenciesFingerprintsFromParsedPom";
 export * from "./common/delivery/code/fingerprint/maven/effectivePomExtractor";
+export * from "./common/delivery/code/fingerprint/node/PackageLockFingerprinter";
 export * from "./common/delivery/code/review/ReviewerError";
 export * from "./common/delivery/code/review/ReviewerRegistration";
 export * from "./common/delivery/code/review/executeReview";
