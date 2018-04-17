1e5f50bcd9de20829df6d976ee0ade9b0d97cb8f
Jessica Kerr <jessitron@gmail.com>
2018-04-17 00:34:45 -0500

    Check the push test in a PushRule

--- test/common/listener/pushRuleTest.ts
+++ test/common/listener/pushRuleTest.ts
@@ -1,19 +1,34 @@
 import {InMemoryProject} from "@atomist/automation-client/project/mem/InMemoryProject";
 import * as assert from "power-assert";
-import { IsLein, LibraryGoals, PushListenerInvocation, whenPushSatisfies } from "../../../src";
+import { IsLein, LibraryGoals, PushListenerInvocation, PushRules, whenPushSatisfies } from "../../../src";
 
 describe("Construction of PushRules", () => {
 
     it("Does not think an empty project is a lein project", async () => {
-        const rule = whenPushSatisfies(IsLein)
+        const rule = new PushRules("test", [whenPushSatisfies(IsLein)
             .itMeans("Build a Clojure library")
-            .setGoals(LibraryGoals);
+            .setGoals(LibraryGoals)]);
 
         const project = InMemoryProject.from({owner: "yes", repo: "no"},
             {path: "package.json", content: "{}"});
-        const result = await rule.valueForPush({project} as any as PushListenerInvocation);
+        const fakePush = {id: "test1"};
+        const result = await rule.valueForPush({project, push: fakePush} as any as PushListenerInvocation);
 
         assert(!result);
+
+    });
+
+    it("Does think a lein project is a lein project", async () => {
+        const rule = new PushRules("test", [whenPushSatisfies(IsLein)
+            .itMeans("Build a Clojure library")
+            .setGoals(LibraryGoals)]);
+
+        const project = InMemoryProject.from({owner: "yes", repo: "no"},
+            {path: "project.clj", content: "{}"});
+        const fakePush = {id: "ttest2"};
+        const result = await rule.valueForPush({project, push: fakePush} as any as PushListenerInvocation);
+
+        assert(result);
 
     });
 });
