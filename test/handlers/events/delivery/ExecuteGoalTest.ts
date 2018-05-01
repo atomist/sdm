d105522443451fe2f6c6ddc1f4b84287c0feb656
Jessica Kerr <jessitron@gmail.com>
2018-05-01 13:49:02 -0500

    LogFactory now takes an SdmGoal (BREAKING)

--- test/handlers/events/delivery/ExecuteGoalTest.ts
+++ test/handlers/events/delivery/ExecuteGoalTest.ts
@@ -47,7 +47,7 @@ describe("executing the goal", () => {
     it("calls a pre-hook and sends output to the log", done => {
         const projectLoader = new SingleProjectLoader(InMemoryProject.of());
 
-        createEphemeralProgressLog("test").then(progressLog => {
+        createEphemeralProgressLog({ name: "test"} as SdmGoal).then(progressLog => {
             const fakeRWLC = {
                 context: fakeContext(),
                 progressLog,
