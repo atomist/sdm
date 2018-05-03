b7ba1dd5fa61d8e0895c96e2af6dbe3ee0a91b41
Christian Dupuis <cd@atomist.com>
2018-05-03 10:50:04 +0200

    fix license header

--- src/common/delivery/goals/storeGoals.ts
+++ src/common/delivery/goals/storeGoals.ts
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,7 +38,7 @@ export interface UpdateSdmGoalParams {
     approved?: boolean;
     error?: Error;
     data?: string;
-}
+}                        http://www.apache.org/licenses/LICENSE-2.0
 
 export function updateGoal(ctx: HandlerContext, before: SdmGoal, params: UpdateSdmGoalParams) {
     const description = params.description;
