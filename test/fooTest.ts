3e8b72b898233c1c09eb2e93a5009409f066cded
Jessitron from loaner <jessica@atomist.com>
2018-05-24 14:30:56 -0500

    This is not working

---
+++ test/fooTest.ts
@@ -0,0 +1,13 @@
+import  "mocha";
+
+console.log("hello world");
+
+describe("things", () => {
+    it("does stuff", () => {
+        console.log("hello");
+    })
+})
+
+export class Whaaa {
+    
+}
