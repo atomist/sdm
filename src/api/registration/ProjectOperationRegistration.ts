de1ca07b200a5b4a20801b82d4314123932013e4
Rod Johnson <rod@atomist.com>
2018-05-31 14:33:58 +1000

    Project creation operations can now take an editor, not just a function

--- src/api/registration/ProjectOperationRegistration.ts
+++ src/api/registration/ProjectOperationRegistration.ts
@@ -3,14 +3,20 @@ import { CommandRegistration } from "./CommandRegistration";
 
 /**
  * Superclass for all registrations of "project operations",
- * which can create or modify projects.
+ * which can create or modify projects. Either an editor or a createEditor
+ * function must be provided.
  */
 export interface ProjectOperationRegistration<PARAMS> extends CommandRegistration<PARAMS> {
+
+    /**
+     * Editor
+     */
+    editor?: AnyProjectEditor<PARAMS>;
 
     /**
      * Create the editor function that can modify a project
      * @param {PARAMS} params
      * @return {AnyProjectEditor}
      */
-    createEditor: (params: PARAMS) => AnyProjectEditor;
+    createEditor?: (params: PARAMS) => AnyProjectEditor<PARAMS>;
 }
--- src/api/registration/support/commandRegistrations.ts
+++ src/api/registration/support/commandRegistrations.ts
@@ -1,4 +1,5 @@
 import { HandleCommand } from "@atomist/automation-client";
+import { AnyProjectEditor } from "@atomist/automation-client/operations/edit/projectEditor";
 import { Maker } from "@atomist/automation-client/util/constructionUtils";
 import { dryRunEditorCommand } from "../../../pack/dry-run/dryRunEditorCommand";
 import { editorCommand } from "../../command/editor/editorCommand";
@@ -8,12 +9,13 @@ import { MachineOrMachineOptions } from "../../machine/support/toMachineOptions"
 import { CommandHandlerRegistration } from "../CommandHandlerRegistration";
 import { EditorRegistration } from "../EditorRegistration";
 import { GeneratorRegistration } from "../GeneratorRegistration";
+import { ProjectOperationRegistration } from "../ProjectOperationRegistration";
 
 export function editorRegistrationToCommand(sdm: MachineOrMachineOptions, e: EditorRegistration<any>): Maker<HandleCommand> {
     const fun = e.dryRun ? dryRunEditorCommand : editorCommand;
     return () => fun(
         sdm,
-        e.createEditor,
+        toEditorFunction(e),
         e.name,
         e.paramsMaker,
         e,
@@ -24,7 +26,7 @@ export function editorRegistrationToCommand(sdm: MachineOrMachineOptions, e: Edi
 export function generatorRegistrationToCommand(sdm: MachineOrMachineOptions, e: GeneratorRegistration<any>): Maker<HandleCommand> {
     return () => generatorCommand(
         sdm,
-        e.createEditor,
+        toEditorFunction(e),
         e.name,
         e.paramsMaker,
         e,
@@ -39,4 +41,14 @@ export function commandHandlerRegistrationToCommand(sdm: MachineOrMachineOptions
         e.paramsMaker,
         e,
     );
+}
+
+function toEditorFunction<PARAMS>(por: ProjectOperationRegistration<PARAMS>): (params: PARAMS) => AnyProjectEditor<PARAMS> {
+    if (!!por.editor) {
+        return () => por.editor;
+    }
+    if (!!por.createEditor) {
+        return por.createEditor;
+    }
+    throw new Error(`Registration '${por.name}' is invalid, as it does not specify an editor or createEditor function`);
 }
