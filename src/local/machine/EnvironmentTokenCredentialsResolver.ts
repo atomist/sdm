93f547088c418fd28a04b018531eaa40a88cb3ac
Rod Johnson <rod@atomist.com>
2018-06-05 16:51:18 +1000

    Generation in progress

---
+++ src/local/machine/EnvironmentTokenCredentialsResolver.ts
@@ -0,0 +1,23 @@
+import { CredentialsResolver } from "../..";
+import { ProjectOperationCredentials } from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
+
+export const EnvironmentTokenCredentialsResolver: CredentialsResolver = {
+
+    eventHandlerCredentials() {
+        return credentialsFromEnvironment();
+    },
+
+    commandHandlerCredentials() {
+        return credentialsFromEnvironment();
+    },
+
+};
+
+function failBecause(msg: string): string {
+    throw new Error(msg);
+}
+
+function credentialsFromEnvironment(): ProjectOperationCredentials {
+    const token = process.env.GITHUB_TOKEN || failBecause("GITHUB_TOKEN must be set");
+    return {token};
+}
