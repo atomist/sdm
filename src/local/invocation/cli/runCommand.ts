2c323e55fcd2fb7ea283b5def101545dfd25edaa
Rod Johnson <rod@atomist.com>
2018-06-06 16:40:46 +1000

    CLI work

--- src/local/invocation/cli/runCommand.ts
+++ src/local/invocation/cli/runCommand.ts
@@ -1,42 +1,82 @@
 #! /usr/bin/env node
 
-import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
-import { execSync } from "child_process";
-import * as fs from "fs";
-import { addGitHooks } from "../../setup/addGitHooks";
 import { sdm } from "../machine";
+import * as fs from "fs";
+import { execSync } from "child_process";
+import { addGitHooks } from "../../setup/addGitHooks";
+import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
 
 /* tslint:disable */
 
-const args = process.argv.slice(2);
+require("yargs")
+    .command({
+        command: "install",
+        desc: "Install web hooks",
+        handler: () => {
+            sdm.installGitHooks();
+        }
+    })
+    .command({
+        command: "import",
+        desc: "Import from GitHub: slalom import --owner=x --repo=y",
+        builder: {
+            owner: {
+                required: true,
+            },
+            repo: {
+                required: true,
+            },
+        },
+        handler: argv => {
+            importFromGitHub(argv.owner, argv.repo);
+        }
+    })
+    .command({
+        command: "generate",
+        aliases: ["g"],
+        builder: {
+            generator: {
+                required: true,
+            },
+            owner: {
+                required: true,
+            },
+            repo: {
+                required: true,
+            },
+        },
+        desc: "Generate",
+        handler: argv => {
+            generate(argv.generator, argv.owner, argv.repo);
+        }
+    })
+    .usage("Usage: $0 <command> [options]")
+    .epilog("Copyright Atomist 2018")
+    .argv;
 
-const command = args[0];
-
-if (args[0] === undefined) {
-    console.log("Usage: slalom [args]");
-    process.exit(1);
+async function importFromGitHub(org: string, repo: string) {
+    console.log(`Adding GitHub project ${org}/${repo}`);
+    const orgDir = `${sdm.configuration.repositoryOwnerParentDirectory}/${org}`;
+    if (!fs.existsSync(orgDir)) {
+        fs.mkdirSync(orgDir);
+    }
+    execSync(`git clone http://github.com/${org}/${repo}`,
+        {cwd: orgDir});
+    addGitHooks(new GitHubRepoRef(org, repo), `${orgDir}/${repo}`);
 }
 
-switch (command) {
-    case "install" :
-        sdm.installGitHooks();
-        break;
+async function generate(commandName: string, targetOwner: string, targetRepo: string) {
+    const hm = sdm.commandMetadata(commandName);
+    if (!hm) {
+        console.log(`No generator with name [${commandName}]: Known commands are [${sdm.commandsMetadata.map(m => m.name)}]`);
+        process.exit(1);
+    }
+    const args = [
+        {name: "target.owner", value: targetOwner},
+        {name: "target.repo", value: targetRepo},
+    ];
 
-    case "import" :
-        if (args.length !== 3) {
-            console.log("Usage: slalom import [org] [repo]");
-            process.exit(1);
-        }
-        const org = args[1];
-        const repo = args[2];
-        console.log(`Adding GitHub project ${org}/${repo}`);
-        const orgDir = `${sdm.configuration.repositoryOwnerParentDirectory}/${org}`;
-        fs.mkdirSync(orgDir);
-        execSync(`git clone http://github.com/${org}/${repo}`,
-            {cwd: orgDir});
-        addGitHooks(new GitHubRepoRef(org, repo), `${orgDir}/${repo}`);
-        break;
-
-    default:
-        console.log(`Unknown command [${command}]`);
+    // TODO should come from environment
+    args.push({name: "github://user_token?scopes=repo,user:email,read:user", value: null});
+    sdm.executeCommand(commandName, args);
 }
