479e57b76bfbcc85f07cb6506fcb2a9fa9cb9fd7
Rod Johnson <rod@atomist.com>
2018-06-06 15:39:57 +1000

    Add configure function

--- src/local/invocation/machine.ts
+++ src/local/invocation/machine.ts
@@ -1,41 +1,11 @@
-import { logger } from "@atomist/automation-client";
-import { whenPushSatisfies } from "../../api/dsl/goalDsl";
-import { FingerprintGoal, PushReactionGoal, ReviewGoal } from "../../api/machine/wellKnownGoals";
-import { hasFileWithExtension } from "../../api/mapping/support/commonPushTests";
-import { TypedFingerprint } from "../../code/fingerprint/TypedFingerprint";
-import { WellKnownGoals } from "../../pack/well-known-goals/addWellKnownGoals";
-import { LogFingerprint } from "../io/logFingerprint";
+import { configure } from "../configure";
 import { LocalSoftwareDeliveryMachine } from "../machine/LocalSoftwareDeliveryMachine";
 import { localSoftwareDeliveryMachineOptions } from "../machine/localSoftwareDeliveryMachineConfiguration";
 
 export const RepositoryOwnerParentDirectory = process.env.LOCAL_SDM_BASE || "/Users/rodjohnson/temp/local-sdm";
 
-// TODO need to put this in a function that takes SDM
-
 export const sdm = new LocalSoftwareDeliveryMachine(
     "gitMachine",
-    localSoftwareDeliveryMachineOptions(RepositoryOwnerParentDirectory),
-    whenPushSatisfies(() => true).setGoals([
-        FingerprintGoal, PushReactionGoal, ReviewGoal,
-    ]));
+    localSoftwareDeliveryMachineOptions(RepositoryOwnerParentDirectory));
 
-sdm
-    .addFingerprintListeners(LogFingerprint)
-    .addExtensionPacks(WellKnownGoals)
-    .addFingerprinterRegistrations({
-        name: "fp1",
-        action: async pu => {
-            const fp = new TypedFingerprint("name", "NM", "0.1.0", {name: "tom"});
-            logger.info("Computed fingerprint %j", fp);
-            return fp;
-        },
-    })
-    .addPushReactions(async p => p.addressChannels("Gotcha!"))
-    .addPushReactions({
-        name: "thing",
-        pushTest: hasFileWithExtension("md"),
-        action: async pu => {
-            const hasReadme = !!(await pu.project.getFile("README.md"));
-            return pu.addressChannels(`Project at ${pu.id.url} has readme=${hasReadme}`);
-        },
-    });
+configure(sdm);
