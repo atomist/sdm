ae17939b4e1987121d22aca71da7d607c6cf7c9a
Rod Johnson <rod@atomist.com>
2018-06-06 14:41:21 +1000

    Fix #392: appendOrCreate should be idempotent

--- src/util/project/appendOrCreate.ts
+++ src/util/project/appendOrCreate.ts
@@ -1,21 +1,47 @@
 import { SimpleProjectEditor } from "@atomist/automation-client/operations/edit/projectEditor";
 
+export interface AppendOrCreateCommand {
+
+    /**
+     * Path of file to create or append to
+     */
+    path: string;
+
+    /**
+     * Content to append. Should include any whitespace
+     * required before it
+     */
+    toAppend: string;
+
+    /**
+     * Custom test as to whether we should create
+     * @param {string} content
+     * @return {boolean}
+     */
+    leaveAlone?: (oldContent: string) => boolean;
+}
+
 /**
  * Return an editor to append the given content to the end of the file at the specified path,
  * creating the file with only this content if it doesn't exist.
  * Adds no whitespace.
- * @param {string} content content to append. Should include any whitespace
- * required before it
- * @param {string} path path of the file to append to or create
+ * @param command command
  * @return {SimpleProjectEditor}
  */
-export function appendOrCreateFileContent(content: string, path: string): SimpleProjectEditor {
+export function appendOrCreateFileContent(command: AppendOrCreateCommand): SimpleProjectEditor {
+    const commandToUse: AppendOrCreateCommand = {
+        leaveAlone: content => content.includes(command.toAppend),
+        ...command,
+    };
     return async p => {
-        const target = await p.getFile(path);
+        const target = await p.getFile(commandToUse.path);
         if (!!target) {
-            await target.setContent(await target.getContent() + content);
+            const oldContent = await target.getContent();
+            if (!commandToUse.leaveAlone(oldContent)) {
+                await target.setContent(oldContent + commandToUse.toAppend);
+            }
         } else {
-            await p.addFile(path, content);
+            await p.addFile(commandToUse.path, commandToUse.toAppend);
         }
         return p;
     };
--- test/util/project/appendOrCreateTest.ts
+++ test/util/project/appendOrCreateTest.ts
@@ -6,7 +6,7 @@ describe("appendOrCreate", () => {
 
     it("should create if doesn't exist", async () => {
         const p = InMemoryProject.of();
-        await appendOrCreateFileContent("content", "Thing1")(p);
+        await appendOrCreateFileContent({toAppend: "content", path: "Thing1"})(p);
         const f = await p.getFile("Thing1");
         assert(!!f, "File should have been created");
         assert.equal(f.getContentSync(), "content");
@@ -14,7 +14,37 @@ describe("appendOrCreate", () => {
 
     it("should append if does exist", async () => {
         const p = InMemoryProject.of({path: "Thing1", content: "dogs"});
-        await appendOrCreateFileContent("content", "Thing1")(p);
+        await appendOrCreateFileContent({toAppend: "content", path: "Thing1"})(p);
+        const f = await p.getFile("Thing1");
+        assert(!!f, "File should still exist");
+        assert.equal(f.getContentSync(), "dogscontent");
+    });
+
+    it("should be idempotent: default literal", async () => {
+        const p = InMemoryProject.of({path: "Thing1", content: "dogscontent"});
+        await appendOrCreateFileContent({toAppend: "content", path: "Thing1"})(p);
+        const f = await p.getFile("Thing1");
+        assert(!!f, "File should still exist");
+        assert.equal(f.getContentSync(), "dogscontent");
+    });
+
+    it("should be idempotent: custom test (yes)", async () => {
+        const p = InMemoryProject.of({path: "Thing1", content: "dogs"});
+        await appendOrCreateFileContent({
+            toAppend: "content", path: "Thing1",
+            leaveAlone: oldContent => oldContent.includes("dog"),
+        })(p);
+        const f = await p.getFile("Thing1");
+        assert(!!f, "File should still exist");
+        assert.equal(f.getContentSync(), "dogs");
+    });
+
+    it("should be idempotent: custom test (no)", async () => {
+        const p = InMemoryProject.of({path: "Thing1", content: "dogs"});
+        await appendOrCreateFileContent({
+            toAppend: "content", path: "Thing1",
+            leaveAlone: oldContent => oldContent.includes("cats"),
+        })(p);
         const f = await p.getFile("Thing1");
         assert(!!f, "File should still exist");
         assert.equal(f.getContentSync(), "dogscontent");
