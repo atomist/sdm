d7c2f8eef77258b102b4abe81d9d5f5e5a4e3e99
Rod Johnson <rod@atomist.com>
2018-06-06 15:39:57 +1000

    Add configure function

---
+++ src/local/configure.ts
@@ -0,0 +1,39 @@
+import { logger } from "@atomist/automation-client";
+import { SoftwareDeliveryMachine } from "..";
+import { whenPushSatisfies } from "../api/dsl/goalDsl";
+import { FingerprintGoal, PushReactionGoal, ReviewGoal } from "../api/machine/wellKnownGoals";
+import { hasFileWithExtension } from "../api/mapping/support/commonPushTests";
+import { TypedFingerprint } from "../code/fingerprint/TypedFingerprint";
+import { WellKnownGoals } from "../pack/well-known-goals/addWellKnownGoals";
+import { LogFingerprint } from "./io/logFingerprint";
+
+/**
+ * User-specific code
+ * @param {} sdm
+ */
+export function configure(sdm: SoftwareDeliveryMachine) {
+    sdm.addGoalContributions(
+        whenPushSatisfies(() => true).setGoals([
+            FingerprintGoal, PushReactionGoal, ReviewGoal,
+        ]));
+    sdm
+        .addFingerprintListeners(LogFingerprint)
+        .addExtensionPacks(WellKnownGoals)
+        .addFingerprinterRegistrations({
+            name: "fp1",
+            action: async pu => {
+                const fp = new TypedFingerprint("name", "NM", "0.1.0", {name: "tom"});
+                logger.info("Computed fingerprint %j", fp);
+                return fp;
+            },
+        })
+        .addPushReactions(async p => p.addressChannels("Gotcha!"))
+        .addPushReactions({
+            name: "thing",
+            pushTest: hasFileWithExtension("md"),
+            action: async pu => {
+                const hasReadme = !!(await pu.project.getFile("README.md"));
+                return pu.addressChannels(`Project at ${pu.id.url} has readme=${hasReadme}`);
+            },
+        });
+}
