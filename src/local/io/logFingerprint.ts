b3b0b9ba7458d0579db640db7c35f224b6cd4b3e
Rod Johnson <rod@atomist.com>
2018-06-07 10:13:08 +1000

    Polishing

--- src/local/io/logFingerprint.ts
+++ src/local/io/logFingerprint.ts
@@ -1,5 +1,11 @@
 import { logger } from "@atomist/automation-client";
 import { FingerprintListener } from "../../api/listener/FingerprintListener";
 
+/**
+ * Display fingerprints to the user
+ * @param {FingerprintListenerInvocation} fli
+ * @return {Promise<Logger>}
+ * @constructor
+ */
 export const LogFingerprint: FingerprintListener = async fli =>
     logger.info("FINGERPRINT: %j", fli.fingerprint);
