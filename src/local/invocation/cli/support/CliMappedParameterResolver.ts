6de7c2a8a87761242a2dea80aa63decaff32dc24
Rod Johnson <rod@atomist.com>
2018-06-07 16:04:35 +1000

    Generators use positional commands. More mapped parameters

--- src/local/invocation/cli/support/CliMappedParameterResolver.ts
+++ src/local/invocation/cli/support/CliMappedParameterResolver.ts
@@ -2,6 +2,9 @@ import { MappedParameters } from "@atomist/automation-client";
 import { MappedParameterDeclaration } from "@atomist/automation-client/metadata/automationMetadata";
 import { parseOwnerAndRepo } from "../../../binding/expandedTreeUtils";
 import { MappedParameterResolver } from "../../../binding/MappedParameterResolver";
+import { MappedRepoParameters } from "@atomist/automation-client/operations/common/params/MappedRepoParameters";
+
+import * as os from "os";
 
 export class CliMappedParameterResolver implements MappedParameterResolver {
 
@@ -13,6 +16,12 @@ export class CliMappedParameterResolver implements MappedParameterResolver {
             case MappedParameters.GitHubOwner :
                 const { owner } = parseOwnerAndRepo(this.repositoryOwnerParentDirectory);
                 return owner;
+            case MappedParameters.SlackTeam :
+                return process.env.SLACK_TEAM || "local";
+            case MappedParameters.SlackUserName :
+                return process.env.SLACK_USER_NAME || os.userInfo().username;
+            case MappedParameters.GitHubWebHookUrl :
+                return "http://not.a.real.url";
         }
         if (!md.required) {
             return undefined;
