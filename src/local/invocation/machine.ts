d31f4b427ffb19735d1d13414aaf74ca6fc03634
Jessica Kerr <jessitron@atomist.com>
2018-06-06 19:44:48 -0500

    fail if environment doesn't define SDM_PROJECTS_ROOT

--- src/local/invocation/machine.ts
+++ src/local/invocation/machine.ts
@@ -2,7 +2,12 @@ import { configure } from "../configure";
 import { LocalSoftwareDeliveryMachine } from "../machine/LocalSoftwareDeliveryMachine";
 import { localSoftwareDeliveryMachineOptions } from "../machine/localSoftwareDeliveryMachineConfiguration";
 
-export const RepositoryOwnerParentDirectory = process.env.SDM_PROJECTS_ROOT || "/Users/rodjohnson/temp/local-sdm";
+function failWith(message: string): string {
+    throw new Error(message)
+}
+
+export const RepositoryOwnerParentDirectory = process.env.SDM_PROJECTS_ROOT ||
+    failWith("Please define SDM_PROJECTS_ROOT to a directory containing git repositories, in the form of owner/repository");
 
 export const sdm = new LocalSoftwareDeliveryMachine(
     "gitMachine",
