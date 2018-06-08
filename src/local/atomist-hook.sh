ac355dad5c674fdacd28bfccb5ec3af0fb879226
Rod Johnson <rod@atomist.com>
2018-06-08 12:33:11 +1000

    Hook is now silent and synchronous

--- src/local/atomist-hook.sh
+++ src/local/atomist-hook.sh
@@ -4,11 +4,11 @@ script=$1
 hookDir=$3
 event=$2
 
-echo "git hook fire: Invoking Atomist $event against $hookDir"
+# echo "git hook fire: Invoking Atomist $event against $hookDir"
 
 node $script\
     $event \
     $hookDir \
     $(git rev-parse --abbrev-ref HEAD) \
     $(git rev-parse HEAD) \
-&
+# &
