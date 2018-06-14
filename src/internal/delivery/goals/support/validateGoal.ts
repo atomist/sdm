c3f1772221996f0427417ea37c885c1ee43d8b21
Christian Dupuis <cd@atomist.com>
2018-06-14 17:14:48 +0200

    Fix goal relevance check when running as a job

--- src/internal/delivery/goals/support/validateGoal.ts
+++ src/internal/delivery/goals/support/validateGoal.ts
@@ -18,7 +18,7 @@ import { automationClientInstance } from "@atomist/automation-client";
 import { SdmGoal } from "../../../../ingesters/sdmGoalIngester";
 
 export function isGoalRelevant(sdmGoal: SdmGoal,
-                               registration: string = automationClientInstance().configuration.name): boolean {
+                               registration: string = process.env.ATOMIST_DEPLOYMENT_NAME || automationClientInstance().configuration.name): boolean {
     // Backwards compatible: we might still have SDMs that don't correctly set the provenance
     if (!sdmGoal.provenance || sdmGoal.provenance.length === 0) {
         return true;
