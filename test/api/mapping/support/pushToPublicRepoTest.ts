f2f53c1c293c564b79c4974b50445059e2600e8e
Jessica Kerr <jessitron@atomist.com>
2018-06-18 13:38:20 -0700

    move toPublicRepo back
    because we don't want to bring ghub into api-helper

--- test/api/mapping/support/pushToPublicRepoTest.ts
+++ test/api/mapping/support/pushToPublicRepoTest.ts
@@ -17,7 +17,7 @@
 import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
 
 import * as assert from "power-assert";
-import { ToPublicRepo } from "../../../../src/api-helper/pushtest/toPublicRepo";
+import { ToPublicRepo } from "../../../../src/mapping/pushtest/toPublicRepo";
 import { PushListenerInvocation } from "../../../../src/api/listener/PushListener";
 
 const credentials = { token: process.env.GITHUB_TOKEN};
