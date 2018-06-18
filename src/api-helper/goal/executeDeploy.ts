0b0fc075d6cac9b9b688de87a006064205a4538b
Jessica Kerr <jessitron@atomist.com>
2018-06-18 13:57:06 -0700

    move executeDeploy

--- src/api-helper/goal/executeDeploy.ts
+++ src/api-helper/goal/executeDeploy.ts
@@ -14,16 +14,22 @@
  * limitations under the License.
  */
 
-import { logger, Success } from "@atomist/automation-client";
-import { ExecuteGoalResult } from "../../../api/goal/ExecuteGoalResult";
-import { Goal } from "../../../api/goal/Goal";
-import { ArtifactStore } from "../../../spi/artifact/ArtifactStore";
-import { checkOutArtifact, setEndpointGoalOnSuccessfulDeploy } from "./deploy";
+import { HandlerContext, logger, Success } from "@atomist/automation-client";
+import { ExecuteGoalResult } from "../../api/goal/ExecuteGoalResult";
+import { Goal } from "../../api/goal/Goal";
+import { ArtifactStore, DeployableArtifact } from "../../spi/artifact/ArtifactStore";
 
 import * as _ from "lodash";
-import { ExecuteGoalWithLog, RunWithLogContext } from "../../../api/goal/ExecuteGoalWithLog";
-import { Target } from "../../../spi/deploy/Target";
-import { RepoRefResolver } from "../../../spi/repo-ref/RepoRefResolver";
+import { ExecuteGoalWithLog, RunWithLogContext } from "../../api/goal/ExecuteGoalWithLog";
+import { Target } from "../../spi/deploy/Target";
+import { RepoRefResolver } from "../../spi/repo-ref/RepoRefResolver";
+import { ProgressLog } from "../../spi/log/ProgressLog";
+import { RemoteRepoRef } from "@atomist/automation-client/operations/common/RepoId";
+import { SdmGoal, SdmGoalState } from "../../ingesters/sdmGoalIngester";
+import { descriptionFromState, updateGoal } from "./storeGoals";
+import { findSdmGoalOnCommit } from "../../internal/delivery/goals/support/fetchGoalsOnCommit";
+import { Deployment } from "../../spi/deploy/Deployment";
+import { ProjectOperationCredentials } from "@atomist/automation-client/operations/common/ProjectOperationCredentials";
 
 /**
  * Execute deploy with the supplied deployer and target
@@ -57,4 +63,72 @@ export function executeDeploy(artifactStore: ArtifactStore,
 
         return Success;
     };
+}
+
+
+export async function checkOutArtifact(targetUrl: string,
+                                       artifactStore: ArtifactStore,
+                                       id: RemoteRepoRef,
+                                       credentials: ProjectOperationCredentials,
+                                       progressLog: ProgressLog): Promise<DeployableArtifact> {
+    if (!targetUrl) {
+        logger.debug("No artifact, must be source-deployed");
+        return sourceArtifact(id);
+    }
+    const artifactCheckout = await artifactStore.checkout(targetUrl, id, credentials)
+        .catch(err => {
+            progressLog.write("Error checking out artifact: " + err.message);
+            throw err;
+        });
+
+    if (!artifactCheckout) {
+        throw new Error("Error checking out artifact: none found");
+    }
+    return artifactCheckout;
+}
+
+function sourceArtifact(id: RemoteRepoRef): DeployableArtifact {
+    return {
+        // TODO need to do something about this: Use general identifier as in PCF editor?
+        name: id.repo,
+        version: "0.1.0",
+        id,
+    };
+}
+
+export async function setEndpointGoalOnSuccessfulDeploy(params: {
+    repoRefResolver: RepoRefResolver,
+    endpointGoal: Goal,
+    rwlc: RunWithLogContext,
+    deployment: Deployment,
+}) {
+    const {rwlc, deployment, endpointGoal} = params;
+    const sdmGoal = await findSdmGoalOnCommit(rwlc.context, rwlc.id, params.repoRefResolver.providerIdFromStatus(rwlc.status), endpointGoal);
+    // Only update the endpoint goal if it actually exists in the goal set
+    if (sdmGoal) {
+        if (deployment.endpoint) {
+            const newState = "success";
+            await markEndpointStatus({context: rwlc.context, sdmGoal, endpointGoal, newState, endpoint: deployment.endpoint});
+        } else {
+            const error = new Error("Deploy finished with success, but the endpoint was not found");
+            const newState = "failure";
+            await markEndpointStatus({context: rwlc.context, sdmGoal, endpointGoal, newState, endpoint: deployment.endpoint, error});
+        }
+    }
+}
+
+function markEndpointStatus(parameters: {
+    context: HandlerContext, sdmGoal: SdmGoal, endpointGoal: Goal, newState: SdmGoalState, endpoint?: string,
+    error?: Error,
+}) {
+    const {context, sdmGoal, endpointGoal, newState, endpoint, error} = parameters;
+    return updateGoal(context, sdmGoal, {
+        description: descriptionFromState(endpointGoal, newState),
+        url: endpoint,
+        state: newState,
+        error,
+    }).catch(endpointStatus => {
+        logger.error("Could not set Endpoint status: " + endpointStatus.message);
+        // do not fail this whole handler
+    });
 }
