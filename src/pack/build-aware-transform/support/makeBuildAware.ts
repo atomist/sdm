651f12bfd79dd68dac776423315196039549a4e6
Rod Johnson <rod@atomist.com>
2018-07-07 18:23:53 +1000

    Suppress raising a PR if previous EditMode wants to

--- src/pack/build-aware-transform/support/makeBuildAware.ts
+++ src/pack/build-aware-transform/support/makeBuildAware.ts
@@ -1,27 +1,10 @@
-/*
- * Copyright © 2018 Atomist, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-import { toEditModeFactory } from "@atomist/automation-client/operations/edit/editModes";
+import { isPullRequest, toEditModeFactory } from "@atomist/automation-client/operations/edit/editModes";
 import { CodeTransformRegistration, CodeTransformRegistrationDecorator } from "../../../api/registration/CodeTransformRegistration";
 
 export const DryRunMessage = "[atomist-dry-run]";
 
 /**
- * Return a function wrapping a CodeTransform function to make
+ * Return a function wrapping a CodeTransform registration to make
  * it build aware: That is, perform a dry run branch push first
  * and create a PR or issue depending on the build result.
  * @return {CodeTransformRegistration}
@@ -35,13 +18,18 @@ export const makeBuildAware: CodeTransformRegistrationDecorator<any> =
         if (!!ctr.editMode) {
             const registeredEm = toEditModeFactory(ctr.editMode);
             dryRunRegistration.editMode = p => {
-                const oldEm = registeredEm(p);
+                const em = registeredEm(p);
                 // Add a dry run message
-                oldEm.message = `${oldEm.message}\n\n${DryRunMessage}`;
-                return oldEm;
+                em.message = `${em.message}\n\n${DryRunMessage}`;
+                if (isPullRequest(em)) {
+                    // Don't let it raise a PR if it wanted to.
+                    // It will remain a valid BranchCommit
+                    em.title = em.body = undefined;
+                }
+                return em;
             };
         } else {
-            // No edit mode was set. We need one that sets a branch:
+            // No edit mode was set. We need to set one that sets a branch:
             // No PR for now
             dryRunRegistration.editMode = () => {
                 const branch = `${ctr.name}-${new Date().getTime()}`;
