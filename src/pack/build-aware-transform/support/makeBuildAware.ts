0eb2d000b5e36a856a4d81bd44a3761de722835a
Rod Johnson <rod@atomist.com>
2018-07-08 12:34:02 +1000

    Use EditModeSuggestion

--- src/pack/build-aware-transform/support/makeBuildAware.ts
+++ src/pack/build-aware-transform/support/makeBuildAware.ts
@@ -15,8 +15,9 @@
  */
 
 import { HandlerContext } from "@atomist/automation-client";
-import { EditMode, isEditMode, isPullRequest, toEditModeFactory } from "@atomist/automation-client/operations/edit/editModes";
+import { EditMode, isPullRequest, toEditModeFactory } from "@atomist/automation-client/operations/edit/editModes";
 import { Project } from "@atomist/automation-client/project/Project";
+import { EditModeSuggestion } from "../../../api/command/editor/EditModeSuggestion";
 import { CodeTransformRegistration, CodeTransformRegistrationDecorator } from "../../../api/registration/CodeTransformRegistration";
 
 export const DryRunMessage = "[atomist-dry-run]";
@@ -34,25 +35,20 @@ export const makeBuildAware: CodeTransformRegistrationDecorator<any> =
             ...ctr,
         };
         dryRunRegistration.editMode = p => {
-            if (isEditMode(p)) {
-                // Parameters are edit mode. Change them
-                return dryRunOf(p);
-            }
             if (!!ctr.editMode) {
                 const registeredEmf = toEditModeFactory(ctr.editMode);
                 return dryRunOf(registeredEmf(p));
             } else {
                 // No edit mode was set. We need to set one that sets a branch:
                 // No PR for now
-                const branch = `${ctr.name}-${new Date().getTime()}`;
-                const message = dryRunMessage(ctr.description || ctr.name);
+                const branch = (p as EditModeSuggestion).desiredBranchName || `${ctr.name}-${new Date().getTime()}`;
+                const desiredCommitMessage = (p as EditModeSuggestion).desiredCommitMessage || dryRunMessage(ctr.description || ctr.name);
                 return {
                     branch,
-                    message,
+                    message: desiredCommitMessage + "\n\n" + DryRunMessage,
                     afterPersist: afterPersistFactory({
-                        message,
-                        title: message,
-                        body: message,
+                        desiredCommitMessage,
+                        desiredPullRequestTitle: (p as EditModeSuggestion).desiredPullRequestTitle || desiredCommitMessage,
                     }),
                 };
             }
@@ -68,8 +64,8 @@ export const makeBuildAware: CodeTransformRegistrationDecorator<any> =
  */
 function dryRunOf(em: EditMode): EditMode {
     // Add dry run message suffix
-    em.message = dryRunMessage(em.message);
-    em.afterPersist = afterPersistFactory(em);
+    em.afterPersist = afterPersistFactory({ desiredCommitMessage: em.message, desiredPullRequestTitle: em.message });
+    em.message = dryRunMessage(em.message) + "\n\n" + DryRunMessage;
     if (isPullRequest(em)) {
         // Don't let it raise a PR if it wanted to.
         // It will remain a valid BranchCommit if it was a PR
@@ -79,12 +75,16 @@ function dryRunOf(em: EditMode): EditMode {
 }
 
 function dryRunMessage(oldMessage: string): string {
-    return `Try to ${oldMessage}\n\n${DryRunMessage}`;
+    return `Try to ${oldMessage}`;
 }
 
-function afterPersistFactory(em: { message: string, title?: string, body?: string }): (p: Project, ctx: HandlerContext) => Promise<any> {
+function afterPersistFactory(ems: {
+    desiredPullRequestTitle?: string,
+    desiredCommitMessage?: string,
+}): (p: Project, ctx: HandlerContext) => Promise<any> {
     return async (p: Project, ctx: HandlerContext) => {
         // TODO raise custom event here using info on edit mode
+        process.stdout.write(`Persist custom event: message='${ems.desiredCommitMessage}', title='${ems.desiredPullRequestTitle}'\n`);
         return undefined;
     };
 }
