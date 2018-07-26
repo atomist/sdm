ba61890815b029a502ca16398a6670242df0e15e
Christian Dupuis <cd@atomist.com>
2018-07-27 00:09:42 +0200

    Fix imports

--- src/api-helper/voter/githubTeamVoter.ts
+++ src/api-helper/voter/githubTeamVoter.ts
@@ -18,29 +18,30 @@ import { TokenCredentials } from "@atomist/automation-client/operations/common/P
 import { isGitHubTeamMember } from "@atomist/automation-client/secured";
 import * as _ from "lodash";
 import {
-    GitHubLogin,
     GoalApprovalRequestVote,
     GoalApprovalRequestVoteResult,
     GoalApprovalRequestVoterRegistration,
-} from "../..";
+} from "../../api/registration/GoalApprovalRequestVoter";
+import { GitHubLogin } from "../../typings/types";
 
 function gitHubTeamVote(team: string): GoalApprovalRequestVote {
     return async gai => {
         const approval = gai.goal.approval;
+        const repo = gai.goal.repo;
 
         const result = await gai.context.graphClient.query<GitHubLogin.Query, GitHubLogin.Variables>({
             name: "GitHubLogin",
             variables: {
                 userId: approval.userId,
-                owner: gai.goal.repo.owner,
-                providerId: gai.goal.repo.providerId,
+                owner: repo.owner,
+                providerId: repo.providerId,
             },
         });
 
         const login = _.get(result, "Team[0].chatTeams[0].members[0].person.gitHubId.login", approval.userId);
         const apiUrl = _.get(result, "Team[0].orgs[0].provider.apiUrl");
 
-        if (await isGitHubTeamMember(gai.goal.repo.owner, login, team, (gai.credentials as TokenCredentials).token, apiUrl)) {
+        if (await isGitHubTeamMember(repo.owner, login, team, (gai.credentials as TokenCredentials).token, apiUrl)) {
             return GoalApprovalRequestVoteResult.Granted;
         } else {
             return GoalApprovalRequestVoteResult.Denied;
