b026cdd2333ee5c50b7ba0449adb531e6b49941d
Christian Dupuis <cd@atomist.com>
2018-08-22 23:22:51 +0200

    More logging

--- src/api-helper/project/CachingProjectLoader.ts
+++ src/api-helper/project/CachingProjectLoader.ts
@@ -78,13 +78,15 @@ export class CachingProjectLoader implements ProjectLoader {
 }
 
 function cleanUp(p: GitProject): void {
-    if (p.baseDir && fs.accessSync(p.baseDir)) {
+    if (p.baseDir && fs.existsSync(p.baseDir)) {
         logger.debug(`Evicting project '%j' at '$s'`, p.id, p.baseDir);
         try {
             fs.removeSync(p.baseDir);
         } catch (err) {
             logger.warn(err);
         }
+    } else {
+        logger.debug(`Evicting project '%j' at '$s' not attempted`, p.id, p.baseDir);
     }
 }
 
