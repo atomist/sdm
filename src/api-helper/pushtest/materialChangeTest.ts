d43b097768730c890a7f8f1a94038681c7b9f6b5
Christian Dupuis <cd@atomist.com>
2018-09-13 23:37:55 +0200

    Add generic IsMaterialChange pushTest

---
+++ src/api-helper/pushtest/materialChangeTest.ts
@@ -0,0 +1,96 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { logger } from "@atomist/automation-client";
+import * as minimatch from "minimatch";
+import { DefaultGoalNameGenerator } from "../../api/goal/GoalNameGenerator";
+import {
+    pushTest,
+    PushTest,
+} from "../../api/mapping/PushTest";
+import {
+    anyFileChangedSuchThat,
+    anyFileChangedWithExtension,
+    filesChangedSince,
+} from "../misc/git/filesChangedSince";
+
+/**
+ * Options to pass to the IsMaterialChange push test
+ */
+export interface MaterialChangeOptions {
+
+    /**
+     * File extensions to watch for
+     */
+    extensions?: string[];
+
+    /**
+     * File paths to watch for
+     */
+    files?: string[];
+
+    /**
+     * Directory paths to watch for
+     * Note: This matches on subdirectories
+     */
+    directories?: string[];
+
+    /**
+     * Glob patters not watch for
+     */
+    globs?: string[];
+}
+
+/**
+ * Check if a Push represents a material change.
+ *
+ * Material changes are changes to files that should trigger certain activity
+ * or goals. Often simple changes to MD or html files should not trigger a full CI/CD process.
+ */
+export function isMaterialChange(options: MaterialChangeOptions = {}): PushTest {
+    return pushTest(DefaultGoalNameGenerator.generateName("material-change"), async pci => {
+
+        const changedFiles = await filesChangedSince(pci.project, pci.push);
+
+        if (!changedFiles) {
+            logger.info(
+                "Cannot determine if change is material on '%s/%s'. Failed to enumerate changed files",
+                pci.id.owner,
+                pci.id.repo);
+            return true;
+        }
+
+        logger.debug(`Changed files: '${changedFiles.join(", ")}'`);
+        if (anyFileChanged(options, changedFiles)) {
+            logger.debug("Change is material on '%s/%s'", pci.id.owner, pci.id.repo);
+            return true;
+        } else {
+            logger.debug("Change is immaterial on '%s/%s'", pci.id.owner, pci.id.repo);
+        }
+        return false;
+    });
+}
+
+export function anyFileChanged(options: MaterialChangeOptions = {},
+                               changedFiles: string[]): boolean {
+    return anyFileChangedWithExtension(changedFiles, options.extensions || []) ||
+        anyFileChangedSuchThat(changedFiles,
+            path => (options.files || []).some(f => path === f)) ||
+        anyFileChangedSuchThat(changedFiles,
+            path => (options.directories || []).some(d => path.startsWith(d))) ||
+        anyFileChangedSuchThat(changedFiles,
+            path => (options.globs || []).some(g => minimatch(path, g)));
+}
---
+++ test/api-helper/pushtest/materialChangeTest.test.ts
@@ -0,0 +1,170 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import assert = require("power-assert");
+import {
+    anyFileChanged,
+    MaterialChangeOptions,
+} from "../../../src/api-helper/pushtest/materialChangeTest";
+
+describe("isMaterialChange", () => {
+
+    describe("anyFileChanged", () => {
+
+        it("should match directories", () => {
+            const options: MaterialChangeOptions = {
+                directories: [".atomist", "src/main"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/bla.java",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should not match directories", () => {
+            const options: MaterialChangeOptions = {
+                directories: ["src/main"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/bla.java",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(!anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should match extensions", () => {
+            const options: MaterialChangeOptions = {
+                extensions: ["json", "java"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/bla.java",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should not match extensions", () => {
+            const options: MaterialChangeOptions = {
+                extensions: ["ts"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/bla.java",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(!anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should match files", () => {
+            const options: MaterialChangeOptions = {
+                files: ["src/test/pom.xml"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/pom.xml",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should not match files", () => {
+            const options: MaterialChangeOptions = {
+                files: ["src/main/pom.xml"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/pom.xml",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(!anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should match globs", () => {
+            const options: MaterialChangeOptions = {
+                globs: ["**/pom.xml"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/pom.xml",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should not match globs", () => {
+            const options: MaterialChangeOptions = {
+                globs: ["**/*.ts"],
+            };
+
+            const changedFiles: string[] = [
+                "src/test/pom.xml",
+                ".atomist/kube/service.json",
+            ];
+
+            assert(!anyFileChanged(options, changedFiles));
+
+        });
+
+        it("should match combination from atomist-sdm", () => {
+            const options: MaterialChangeOptions = {
+                extensions: ["ts", "json", "graphql"],
+                files: ["Dockerfile", ".gitattributes", ".npmignore", ".dockerignore", ".gitignore"],
+                directories: [".atomist/", "legal"],
+            };
+
+            assert(anyFileChanged(options, [
+                "src/test/atomist.config.ts",
+                ".atomist/kube/service.json",
+            ]));
+
+            assert(anyFileChanged(options, [
+                "Dockerfile",
+                ".atomist/kube/service.json",
+            ]));
+
+            assert(anyFileChanged(options, [
+                "Dockerfile",
+                ".atomist/kube/service.json",
+                ".gitignore",
+            ]));
+
+            assert(anyFileChanged(options, [
+                "src/test/atomist.config.ts",
+                "legal/kube/service.json",
+            ]));
+        });
+
+    });
+
+});
