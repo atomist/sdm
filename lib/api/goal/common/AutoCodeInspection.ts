25ca60a16aa05eed9f4c10a265aa5f5d4e81e3fd
Christian Dupuis <cd@atomist.com>
2018-11-09 08:30:01 +0100

    Change reportToSlack default back to true for backwards compatibility

--- lib/api/goal/common/AutoCodeInspection.ts
+++ lib/api/goal/common/AutoCodeInspection.ts
@@ -37,7 +37,7 @@ export interface AutoCodeInspectionOptions {
 }
 
 const DefaultAutoCodeInspectionOptions: AutoCodeInspectionOptions = {
-    reportToSlack: false,
+    reportToSlack: true,
 };
 
 /**
