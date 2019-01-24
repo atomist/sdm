2eca318b0188de53c6afe8c4b5160cbbf42b7173
Christian Dupuis <cd@atomist.com>
2019-01-24 12:34:43 +0100

    Namespace service key with atomist.com

--- lib/api/registration/ServiceRegistration.ts
+++ lib/api/registration/ServiceRegistration.ts
@@ -20,13 +20,13 @@ import { SdmGoalEvent } from "../goal/SdmGoalEvent";
 /**
  * Key under which services can be found in goal data.
  */
-export const ServiceRegistrationGoalDataKey = "sdm/service";
+export const ServiceRegistrationGoalDataKey = "atomist.com/sdm/service";
 
 /**
  * Register additional services for a goal.
  * This can be used to add additional containers into k8s jobs to use during goal execution.
  */
-export interface ServiceRegistration<T> {
-    name: string;
+export interface ServiceRegistration<T, N extends string> {
+    name: N;
     service: (goalEvent: SdmGoalEvent, repo: RepoContext) => Promise<{ type: string, spec: T } | undefined>;
 }
