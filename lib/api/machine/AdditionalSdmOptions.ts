1cfa317d8c66c3dd3e61ed17fdb9e5b660a99fe8
Jessica Kerr <jessitron@atomist.com>
2019-02-26 12:23:45 -0600

    You have to specify the property within sdm

--- lib/api/machine/AdditionalSdmOptions.ts
+++ lib/api/machine/AdditionalSdmOptions.ts
@@ -1,14 +1,15 @@
 
-
 /**
  * Use this to declare that your Configuration includes additional
  * SDM configuration options, based on extension packs or optional functionality.
  */
-export type AdditionalSdmOptions<OptionsType> = {
-    sdm: OptionsType
+export interface AdditionalSdmOptions<OptionsType> {
+    sdm: OptionsType;
 }
 
-export type SdmCacheOptions = {
-    enabled: boolean,
-    path: string,
+export interface SdmCacheOptions {
+    cache: {
+        enabled: boolean;
+        path: string;
+    };
 }
