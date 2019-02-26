0556cf7077ac95c42fa05f84e7af705cfb86aec7
Jessica Kerr <jessitron@atomist.com>
2019-02-26 15:56:51 -0600

    Add apidoc for projectConfigurationValue

--- lib/api-helper/project/configuration/projectConfiguration.ts
+++ lib/api-helper/project/configuration/projectConfiguration.ts
@@ -20,10 +20,19 @@ import {
 } from "@atomist/automation-client";
 import * as _ from "lodash";
 
+/**
+ * Gather a configuration value from:
+ *
+ * 1) within a file .atomist/config.json within the Project
+ * 2) from within the `sdm` property in the wider, globally available configuration. For where these come from, see:
+ *  https://atomist.github.io/automation-client/modules/_lib_configuration_.html?_ga=2.177847404.819470215.1551037765-1203552146.1527824248#loadconfiguration
+ * 3) the default passed in, if any
+ * 4) ... or else throw.
+ */
 export async function projectConfigurationValue<T>(path: string, p: Project, defaultValue?: T): Promise<T> {
     // Project specific configuration first
     const cf = await p.getFile(".atomist/config.json");
-    if (cf)  {
+    if (cf) {
         const conf = JSON.parse(await cf.getContent());
         const value = _.get(conf, path) as T;
         if (value !== undefined) {
