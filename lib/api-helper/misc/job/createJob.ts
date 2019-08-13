2fe38306feb63a682e9916cb98b2a5a5b0e90701
Christian Dupuis <cd@atomist.com>
2019-08-13 22:32:01 +0200

    Fix no parameters issue with createJob

--- lib/api-helper/misc/job/createJob.ts
+++ lib/api-helper/misc/job/createJob.ts
@@ -67,7 +67,10 @@ export async function createJob<T extends ParameterType>(details: JobDetails<T>,
     data.secrets = [];
 
     const cmd = typeof command === "string" ? command : command.name;
-    const params = Array.isArray(parameters) ? parameters : [parameters];
+    let params = (Array.isArray(parameters) ? parameters : [parameters]).filter(p => !!p);
+    if (params.length === 0) {
+       params = [{}] as T[];
+    }
 
     const result = await ctx.graphClient.mutate<CreateJob.Mutation, CreateJob.Variables>({
         name: "CreateJob",
