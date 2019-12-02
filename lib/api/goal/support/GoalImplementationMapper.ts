07ba07f306bcc1fc0141741a790af5ec9da4647b
Christian Dupuis <cd@atomist.com>
2019-12-02 12:49:23 +0000

    Autofix: TypeScript header
    [atomist:generated] [atomist:autofix=typescript_header]

--- lib/api/goal/support/GoalImplementationMapper.ts
+++ lib/api/goal/support/GoalImplementationMapper.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 Atomist, Inc.
+ * Copyright © 2019 Atomist, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,6 +67,8 @@ export interface GoalFulfillmentCallback {
  * Registers and looks up goal implementations
  */
 export interface GoalImplementationMapper {
+
+    hasImplementation(): boolean;
 
     addSideEffect(sideEffect: GoalSideEffect): this;
 
