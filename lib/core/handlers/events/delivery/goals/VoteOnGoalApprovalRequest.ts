e4d47f767c2e3b2a17f23309c46822f86e67c975
Christian Dupuis <cd@atomist.com>
2021-05-05 10:40:46 +0200

    Add support for signing custom events

--- lib/core/handlers/events/delivery/goals/VoteOnGoalApprovalRequest.ts
+++ lib/core/handlers/events/delivery/goals/VoteOnGoalApprovalRequest.ts
@@ -89,14 +89,14 @@ export class VoteOnGoalApprovalRequest implements HandleEvent<OnAnyApprovedSdmGo
 
     public async handle(event: EventFired<OnAnyApprovedSdmGoal.Subscription>,
                         context: HandlerContext): Promise<HandlerResult> {
-        const sdmGoal = event.data.SdmGoal[0] as SdmGoalEvent;
+        let sdmGoal = event.data.SdmGoal[0] as SdmGoalEvent;
 
         if (!shouldHandle(sdmGoal)) {
             logger.debug(`Goal ${sdmGoal.name} skipped because not managed by this SDM`);
             return Success;
         }
 
-        await verifyGoal(sdmGoal, this.configuration.sdm.goalSigning, context);
+        sdmGoal  =await verifyGoal(sdmGoal, this.configuration.sdm.goalSigning, context);
 
         const id = this.repoRefResolver.repoRefFromPush(sdmGoal.push);
         const credentials = await resolveCredentialsPromise(this.credentialsFactory.eventHandlerCredentials(context, id));
---
+++ lib/core/signing/jwtGoalSigning.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2020 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as crypto from "crypto";
+import * as _ from "lodash";
+import { SdmGoalEvent } from "../../api/goal/SdmGoalEvent";
+import { SdmGoalMessage } from "../../api/goal/SdmGoalMessage";
+import {
+    GoalSigningAlgorithm,
+    SigningKey,
+    VerificationKey,
+} from "../../api/machine/SigningKeys";
+
+/**
+ * JWT based GoalSigningAlgorithm
+ *
+ * Generate keys:
+ *
+ * openssl ecparam -genkey -name secp521r1 -noout -out es512-private.pem
+ * openssl ec -in es512-private.pem -pubout -out es512-public.pem
+ */
+export const JwtGoalSigningAlgorithm: GoalSigningAlgorithm<string> = {
+
+    name: "jwt-es512",
+
+    sign: async (goal: SdmGoalMessage, key: SigningKey<string>) => {
+        const privateKey = crypto.createPrivateKey({
+            key: key.privateKey,
+            passphrase: key.passphrase,
+        });
+        const { default: CompactSign } = require("jose/jws/compact/sign");
+        const jws = await new CompactSign(Buffer.from(JSON.stringify(goal)))
+            .setProtectedHeader({ alg: "ES512" })
+            .sign(privateKey);
+        return jws;
+    },
+
+    verify: async (goal: SdmGoalEvent, signatureString: string, key: VerificationKey<string>) => {
+        const publicKey = crypto.createPublicKey({
+            key: key.publicKey,
+        });
+        const { default: compactVerify } = require("jose/jws/compact/verify");
+        try {
+            const { payload } = await compactVerify(signatureString, publicKey);
+            return _.merge({}, goal,JSON.parse(Buffer.from(payload).toString())) as SdmGoalEvent
+        } catch (e) {
+            return undefined;
+        }
+    },
+};
